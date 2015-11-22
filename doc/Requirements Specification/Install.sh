d02e64c9c5e70a51c3953a541ffe19f0b9b4e9cd
Joshua Gleitze <git@joshuagleitze.de>
2015-11-22 13:27:28 +0100

    Add LyX PPA to Travis, set Travis language to Java

--- doc/Requirements Specification/Install.sh
+++ doc/Requirements Specification/Install.sh
@@ -4,9 +4,10 @@
 # fail the script if any command fails
 set -e 
 
+# LyX PPA for latest version
+sudo add-apt-repository ppa:lyx-devel/release
 # Install dependency packages
-
-sudo apt-get install texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-fonts-extra texlive-lang-german
+sudo apt-get install -qq texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-fonts-extra texlive-lang-german
 
 # Copy the lyx layout file to the private lyx layouts folder
 mkdir -p ~/.lyx/layouts
@@ -14,7 +15,6 @@ cp ../Dependencies/sdqthesis.layout ~/.lyx/layouts
 
 # Download the sdqthesis template, unzip it, copy the important content into the local texmf, run texhash
 texmf="/usr/share/texmf/tex/latex/sdqthesis"
-sudo mkdir -p $texmf
 
 sdqthesiszip="https://sdqweb.ipd.kit.edu/mediawiki-sdq-extern/images/7/76/Ausarbeitungs-Vorlage_SDQ_2014.zip"
 
