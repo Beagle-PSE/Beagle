9891c8b2c9637891392ca4c973840daf39b41b3e
Joshua Gleitze <git@joshuagleitze.de>
2015-11-22 13:16:51 +0100

    Switch to Travis trusty

--- doc/requirements specification/install.sh
+++ doc/requirements specification/install.sh
@@ -4,13 +4,17 @@
 # fail the script if any command fails
 set -e 
 
+# Install dependency packages
+
+sudo apt-get install texlive-latex-base texlive-latex-recommended texlive-latex-extra texlive-fonts-extra texlive-lang-german
+
 # Copy the lyx layout file to the private lyx layouts folder
 mkdir -p ~/.lyx/layouts
-cp ../dependencies/sdqthesis.layout ~/.lyx/layouts
+cp ../Dependencies/sdqthesis.layout ~/.lyx/layouts
 
 # Download the sdqthesis template, unzip it, copy the important content into the local texmf, run texhash
-texmfhome="$(kpsewhich -var-value=TEXMFHOME)"
-mkdir -p $texmfhome
+texmf="/usr/share/texmf/tex/latex/sdqthesis"
+sudo mkdir -p $texmf
 
 sdqthesiszip="https://sdqweb.ipd.kit.edu/mediawiki-sdq-extern/images/7/76/Ausarbeitungs-Vorlage_SDQ_2014.zip"
 
@@ -18,7 +22,7 @@ tmpdir=`mktemp -d`
 zipname="$tmpdir/sdqthesis.zip"
 
 wget -qO- -O $zipname $sdqthesiszip && unzip $zipname -d $tmpdir
-cp -r $tmpdir/logos $tmpdir/sdqthesis.cls $tmpdir/title-background.pdf $tmpdir/title-background.eps $texmfhome
+sudo cp -r $tmpdir/logos $tmpdir/sdqthesis.cls $tmpdir/title-background.pdf $tmpdir/title-background.eps $texmfhome
 rm -rf $tmpdir
 
-texhash $texmfhome
+sudo texhash
