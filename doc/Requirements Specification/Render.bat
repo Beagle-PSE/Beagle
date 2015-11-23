23cfee71d0e7432bf4d32592d478d354f91d30f0
Joshua Gleitze <git@joshuagleitze.de>
2015-11-24 00:59:07 +0100

    Improve the SRS render scripts

--- doc/Requirements Specification/Render.bat
+++ doc/Requirements Specification/Render.bat
@@ -1,39 +1,83 @@
+:: only print what we want to have printed
+@echo off
+
 :: LyX executable. Add some logic here to search differen paths if your installation is elsewhere. 
 set LYX="C:\Program Files (x86)\LyX 2.1\bin\lyx.exe"
 if not exist %LYX% set LYX="G:\Programme\LyX 2.1\bin\lyx.exe"
 set FILENAME="Requirements Specification"
+
+:: separator for log messages
+set sep=######################################
 
 :: Get a temporary folder name that we can savely delete later
 call :GETTEMPNAME
 :: Create the folder and copy all contents in it
 mkdir %TMPDIR%
 xcopy . %TMPDIR% /e /i
+set log="%TMPDIR%\Render.log"
+set tee="%TMPDIR%\tee"
 
 set drive=%cd:~0,2%
 set OLDPWD=%cd%
 C:
 cd %TMPDIR%
 
+:: The old log was copied in, remove it.
+if exist %log% del %log%
+
+
+:::::::::::::::
+:: Main Part ::
+:::::::::::::::
+
+
 :: generate .tex
-%LYX% --export pdflatex %FILENAME%.lyx
+call :log Rendering with LyX
+%LYX% --export pdflatex %FILENAME%.lyx > %tee% || (call :teeerror & exit /B 1)
+call :tee
+
 :: generate all the other stuff
-pdflatex %FILENAME%.tex
+call :log First pdflatex run
+pdflatex -halt-on-error %FILENAME%.tex >> %log% || (call :logerror & exit /B 1)
+
 :: make the glossary
-makeglossaries %FILENAME%
-bibtex %FILENAME%
+call :log Generating the glossary
+makeglossaries %FILENAME% > %tee% || (call :teeerror & exit /B 1)
+call :tee
+
+:: make the biliography
+call :log Generating the bibliography
+bibtex %FILENAME% > %tee% || (call :teeerror & exit /B 1)
+call :tee
+
 :: render the pdf (we need at least two times. And you know… just to be sure ;))
-pdflatex %FILENAME%.tex
-pdflatex %FILENAME%.tex
-pdflatex %FILENAME%.tex
-pdflatex %FILENAME%.tex
+:: The output of the first runs is irrelevant
+call :log Second pdflatex run
+pdflatex -halt-on-error %FILENAME%.tex >> %log% || (call :logerror & exit /B 1)
+call :log Third pdflatex run
+pdflatex -halt-on-error %FILENAME%.tex >> %log% || (call :logerror & exit /B 1)
+call :log Fourth pdflatex run
+pdflatex -halt-on-error %FILENAME%.tex >> %log% || (call :logerror & exit /B 1)
+call :log Last pdflatex run
+pdflatex -halt-on-error %FILENAME%.tex > %tee% || (call :teeerror & exit /B 1)
+call :tee
+
+::::::::::::::::::::::
+:: End of Main Part ::
+::::::::::::::::::::::
+
+
+
+
 
 :: Go back
 %drive%
 cd %OLDPWD%
 
 
-:: Save the PDF
+:: Save the PDF and log
 copy %TMPDIR%\%FILENAME%.pdf . /y
+copy %log% . /y
 
 
 :: Delete the temporary folder, as we don't need the tons of files in it
@@ -43,5 +87,59 @@ goto :EOF
 :GETTEMPNAME
 set TMPDIR=%TMP%\%RANDOM%
 if exist "%TMPDIR%" GOTO :GETTEMPNAME 
+exit /B 0
+
+:: logs the provided argument to both stdout and the logfile
+:log
+echo( >> %log%
+echo( >> %log%
+echo( >> %log%
+echo %sep% >> %log%
+echo %* >> %log%
+echo %sep% >> %log%
+echo( >> %log%
+echo( >> %log%
+echo(
+echo(
+echo(
+echo %sep%
+echo %*
+echo %sep%
+echo(
+echo(
+exit /B 0
+
+:: prints the contents of %tee% both into the logfile and on stdout (Poor man's tee)
+:tee
+type %tee% >> %log%
+type %tee%
+exit /B 0
+
+
+:: called when a normal logging command failed. Prints the whole log to stdout
+:logerror
+type %log%
+call :errorcleanup
+exit /B 1
+
+:: called when a tee logging command failed. Stores the tee file and continues with :logerror
+:teeerror
+call :tee
+call :logerror
+exit /B 1
+
+:: called on every fail. Restores the PWD
+:: saves the log, prints a messages and exits.
+:errorcleanup
+%drive%
+cd %OLDPWD%
+copy %log% . /y
+echo(
+echo(
+echo(
+echo(
+echo An error occured (see output above).
+echo You can see the files generated during rendering in %TMPDIR% (type "cd %%TMPDIR%%")
+exit /B 1
 
 :EOF
--- doc/Requirements Specification/Render.sh
+++ doc/Requirements Specification/Render.sh
@@ -7,27 +7,86 @@ set -e
 
 # Copy everything to a new folder that we can savely delete
 tmpdir=`mktemp -d`
+log=$tmpdir/Render.log
+
 cp -r * "$tmpdir"
 cd "$tmpdir"
 
-# generate .tex
-lyx -e pdflatex "$file.lyx"
-# generate all the other stuff
-pdflatex -halt-on-error "$file.tex"
-# make the glossary
-makeglossaries "$file"
-bibtex "$file"
-# render the pdf (we need at least two times. And you know… just to be sure ;))
-pdflatex -halt-on-error "$file.tex"
-pdflatex -halt-on-error "$file.tex"
-pdflatex -halt-on-error "$file.tex"
-pdflatex -halt-on-error "$file.tex"
+# The old log was copied in, remove it.
+rm -f $log
 
-# Go back up
+# Called when one of the commands fails. Restores the PWD, writes the full log to stdout,
+# saves the log, prints a messages and exits.
+function error {
+	cat $log
+	cd "$OLDPWD"
+	cp -f $log .
+	echo "
+
+	
+	
+An error occured (see output above).
+You can see the files generated during rendering in $tmpdir"
+	exit 1
+}
+
+function log {
+	echo "
+
+
+######################################
+$1
+######################################
+
+" | tee -a $log
+}
+
+
+
+
+#############
+# Main Part #
+#############
+
+# generate .tex
+log "Rendering with LyX"
+lyx -e pdflatex "$file.lyx" | tee -a $log || error
+
+# generate all the other stuff
+log "First pdflatex run"
+pdflatex -halt-on-error "$file.tex" >> $log || error
+
+# make the glossary
+log "Generating the glossary"
+makeglossaries "$file" | tee -a $log || error
+
+# make the biliography
+log "Generating the bibliography"
+bibtex "$file" | tee -a $log || error
+
+# render the pdf (we need at least two times. And you know… just to be sure ;))
+# The output of the first runs is irrelevant
+log "Second pdflatex run"
+pdflatex -halt-on-error "$file.tex" >> $log || error
+log "Third pdflatex run"
+pdflatex -halt-on-error "$file.tex" >> $log || error
+log "Fourth pdflatex run"
+pdflatex -halt-on-error "$file.tex" >> $log || error
+log "Last pdflatex run"
+pdflatex -halt-on-error "$file.tex" | tee -a $log || error
+
+####################
+# End of Main Part #
+####################
+
+
+
+
+# Go back
 cd "$OLDPWD"
 
-# copy the rendered pdf to save it
-cp -f "$tmpdir/$file.pdf" .
+# copy the rendered pdf and log to save them
+cp -f "$tmpdir/$file.pdf" $log .
 # Delete tmp-render as it contains tons of files we don’t want.
 rm -rf "$tmpdir"
 
