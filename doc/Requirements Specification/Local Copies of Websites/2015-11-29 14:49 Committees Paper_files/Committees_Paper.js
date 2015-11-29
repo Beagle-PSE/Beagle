a0dcb3b8dc2a010e7f90e52285a0c5fd5370e4db
m1cm1c <michelbach@hmamail.com>
2015-11-29 15:11:53 +0100

    Tarred websites.

--- doc/Requirements Specification/Local Copies of Websites/2015-11-29 14:49 Committees Paper_files/Committees_Paper.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20120823
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Committees_Paper_files/Committees_PaperMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
