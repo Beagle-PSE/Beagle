3a16068ab30778c182c0f4b891eec81467b14b0b
Roman Langrehr <roman.langrehr@yahoo.de>
2015-12-25 12:36:30 +0100

    Added a mockup measurment tool.

---
+++ prototypes/Measurment Tool Mockup/src/de/uka/ipd/sdq/beagle/measurmenttoolmockup/MockupMeasurmentTool.java
@@ -0,0 +1,19 @@
+package de.uka.ipd.sdq.beagle.measurmenttoolmockup;
+
+import de.uka.sdq.beagle.measurement.MeasurementOrder;
+import de.uka.sdq.beagle.measurement.MeasurementTool;
+
+/**
+ * An (empty) implementation of
+ * {@link de.uka.sdq.beagle.measurement.MeasurementTool}.
+ *
+ * @author Roman Langrehr
+ */
+public class MockupMeasurmentTool implements MeasurementTool {
+
+	@Override
+	public void measure(final MeasurementOrder measurements) {
+
+	}
+
+}
