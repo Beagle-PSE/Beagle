d1e15341e3579f4568e0f445c1bcebe24a501dd8
Roman Langrehr <roman.langrehr@yahoo.de>
2015-12-25 12:06:14 +0100

    Added a dialog, which shows the available measurment tools.

--- prototypes/Extension Point Prototype/src/de/uka/ipd/sdq/beagle/extensionpointprototype/MeasurementToolContributionsHandler.java
+++ prototypes/Extension Point Prototype/src/de/uka/ipd/sdq/beagle/extensionpointprototype/MeasurementToolContributionsHandler.java
@@ -6,6 +6,10 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -43,5 +47,13 @@ public class MeasurementToolContributionsHandler {
 		} catch (final CoreException exception) {
 			throw new RuntimeException(exception);
 		}
+		String measurmentToolsAsString = "";
+		for (final MeasurementTool measurmentTool : measurementTools) {
+			measurmentToolsAsString += " - " + measurmentTool.getClass().getPackage() + "."
+					+ measurmentTool.getClass().getName() + "\n";
+		}
+		final IWorkbench workbench = PlatformUI.getWorkbench();
+		final IWorkbenchWindow window = workbench.getActiveWorkbenchWindow();
+		MessageDialog.openInformation(window.getShell(), "Beagle is alive!", "Available measurment tools:\n");
 	}
 }
