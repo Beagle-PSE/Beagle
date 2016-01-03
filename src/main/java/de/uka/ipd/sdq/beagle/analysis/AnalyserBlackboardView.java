af4292e5f359e4ab08939d4fb4341ae203344d95
m1cm1c <michelbach94@gmail.com>
2016-01-03 16:28:26 +0100

    Added Javadoc to methods and improved class Javadoc.

--- src/main/java/de/uka/ipd/sdq/beagle/analysis/AnalyserBlackboardView.java
+++ src/main/java/de/uka/ipd/sdq/beagle/analysis/AnalyserBlackboardView.java
@@ -13,7 +13,7 @@ import java.util.Collection;
  */
 
 /**
- * This view of the Blackboard is designed to be used by {@link ResultAnalyser}.
+ * This view of the {@link Blackboard} is designed to be used by {@link ResultAnalyser}.
  * 
  * @author Christoph Michelbach
  */
@@ -27,26 +27,54 @@ public class AnalyserBlackboardView {
 		return null; // TODO Implement method
 	}
 
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllSEFFBranches()}.
+	 *
+	 * @return {@see de.uka.ipd.sdq.beagle.core.Blackboard#getAllSEFFBranches()}
+	 */
 	public Collection<SEFFBranch> getAllSeffBranches() {
 		return null; // TODO Implement method
 	}
 
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffLoops()}.
+	 *
+	 * @return {@see de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffLoops()}
+	 */
 	public Collection<SEFFLoop> getAllSeffLoops() {
 		return null; // TODO Implement method
 	}
 
+	/**
+	 * Adds a result to the {@link Blackboard}.
+	 */
 	public void addAnalyseResult() {
 		// TODO Implement method
 	}
 
+	/**
+	 * Returns the {@link ResourceDemandingInternalAction}s to be measured.
+	 * 
+	 * @return the {@link ResourceDemandingInternalAction}s to be measured
+	 */
 	public Collection<ResourceDemandingInternalAction> getResourceDemandingInternalActionsToBeMeasured() {
 		return null; // TODO Implement method
 	}
 
+	/**
+	 * Returns the {@link SEFFBranch}es to be measured.
+	 * 
+	 * @return the {@link SEFFBranch}es to be measured
+	 */
 	public Collection<SEFFBranch> getSeffBranchesToBeMeasured() {
 		return null; // TODO Implement method
 	}
 
+	/**
+	 * Returns the {@link SEFFLoop}s to be measured.
+	 * 
+	 * @return the {@link SEFFLoop}s to be measured
+	 */
 	public Collection<SEFFLoop> getSeffLoopsToBeMeasured() {
 		return null; // TODO Implement method
 	}
