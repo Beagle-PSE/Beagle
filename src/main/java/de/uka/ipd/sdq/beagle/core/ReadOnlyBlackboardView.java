7c6a1c39236a113c6518567109fd7b946b2c97a2
m1cm1c <michelbach@hmamail.com>
2016-01-03 13:53:28 +0100

    Wrote Javadoc.

--- src/main/java/de/uka/ipd/sdq/beagle/core/ReadOnlyBlackboardView.java
+++ src/main/java/de/uka/ipd/sdq/beagle/core/ReadOnlyBlackboardView.java
@@ -1,19 +1,17 @@
 package de.uka.ipd.sdq.beagle.core;
 
 /*
- * ATTENTION: Checkstyle turned off!
- * remove this comment block when implementing this class!
- *
- * CHECKSTYLE:OFF
- *
- * TODO
+ * ATTENTION: Checkstyle turned off! remove this comment block when implementing this
+ * class! CHECKSTYLE:OFF TODO
  */
 
 /**
- * TODO Document this type.
- *
+ * A view of the Blackboard only allowing reading access.
+ * 
+ * @author Christoph Michelbach
  */
 public class ReadOnlyBlackboardView {
+
 	public ReadOnlyBlackboardView(Blackboard blackboard) {
 		// TODO: Implement method
 	}
