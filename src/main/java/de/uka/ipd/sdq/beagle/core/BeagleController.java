f319f5a8fe2cf6aac3368c2c694f26a4fa70410e
Roman Langrehr <roman.langrehr@yahoo.de>
2016-01-03 22:37:55 +0100

    Added missing links. Fixed typos.

--- src/main/java/de/uka/ipd/sdq/beagle/core/BeagleController.java
+++ src/main/java/de/uka/ipd/sdq/beagle/core/BeagleController.java
@@ -7,8 +7,8 @@ import de.uka.ipd.sdq.beagle.measurement.MeasurementTool;
 /**
  * Controls whether a {@link MeasurementTool}, a {@link ResultAnalyser} or the
  * {@link FinalJudge} is working. If a {@link ResultAnalyser} is working, it also
- * controls, which one. There is always only at most one measurement tool or one result
- * analyser or the final judge working.
+ * controls, which one. There is always only at most one {@link MeasurementTool} or one
+ * {@link ResultAnalyser} or the {@link FinalJudge} working.
  *
  * @author Roman Langrehr
  * @see MeasurementController
@@ -18,7 +18,7 @@ public class BeagleController {
 	/**
 	 * This controller's blackboard, passed to all interacting jobs to communicate. Any
 	 * data exchange in any action invoked by this controller must happen exclusively via
-	 * this blackboard instance.
+	 * this {@link Blackboard} instance.
 	 */
 	private final Blackboard blackboard = new Blackboard();
 
--- src/main/java/de/uka/ipd/sdq/beagle/core/MeasurementController.java
+++ src/main/java/de/uka/ipd/sdq/beagle/core/MeasurementController.java
@@ -20,12 +20,13 @@ public class MeasurementController {
 	private Collection<MeasurementTool> measurementTools;
 
 	/**
-	 * Determines whether a measurement tool can contribute to the blackboard.
+	 * Determines whether a {@link MeasurementTool} can contribute to the
+	 * {@link Blackboard}.
 	 *
 	 * @param blackboard The blackboard
-	 * @return Whether a measurement tool can measure something, which is marked as 'to be
-	 *         measured'. When {@code true} is returned this is no guarantee, that at
-	 *         least one new measurement result will be added.
+	 * @return Whether a {@link MeasurementTool} can measure something which is marked as
+	 *         'to be measured'. When {@code true} is returned, this is no guarantee that
+	 *         at least one new measurement result will be added.
 	 */
 	public boolean canMeasure(final ReadOnlyBlackboardView blackboard) {
 		// implement method
@@ -34,12 +35,13 @@ public class MeasurementController {
 
 	/**
 	 * Instructs all available {@link MeasurementTool MeasurementTools} to measure all
-	 * items marked as “to be measured”. Measurement Tools may not produce results for
-	 * every item but will report results for all items they were able to measure.
+	 * items marked as “to be measured”. {@link MeasurementTool MeasurementTools} may not
+	 * produce results for every item but will report results for all items they were able
+	 * to measure.
 	 *
 	 * <p>This method may only be called, when {@link #canMeasure(ReadOnlyBlackboardView)}
-	 * returned {@code true} before and the blackboard wasn't changed between this call.
-	 * Otherwise the behaviour of this method is undefined.
+	 * returned {@code true} before and the {@link Blackboard} wasn't changed between this
+	 * call. Otherwise the behaviour of this method is undefined.
 	 *
 	 * @param blackboard The blackboard.
 	 */
