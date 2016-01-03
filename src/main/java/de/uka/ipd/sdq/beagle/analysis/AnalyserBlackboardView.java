a383bec976881e6a1b5377d385ee22b93c70acd1
m1cm1c <michelbach@hmamail.com>
2016-01-03 14:00:05 +0100

    Added dummies to AnalyserBlackboardView.

--- src/main/java/de/uka/ipd/sdq/beagle/analysis/AnalyserBlackboardView.java
+++ src/main/java/de/uka/ipd/sdq/beagle/analysis/AnalyserBlackboardView.java
@@ -1,22 +1,51 @@
 package de.uka.ipd.sdq.beagle.analysis;
 
 import de.uka.ipd.sdq.beagle.core.Blackboard;
+import de.uka.ipd.sdq.beagle.core.ResourceDemandingInternalAction;
+import de.uka.ipd.sdq.beagle.core.SEFFBranch;
+import de.uka.ipd.sdq.beagle.core.SEFFLoop;
+
+import java.util.Collection;
 
 /*
- * ATTENTION: Checkstyle turned off!
- * remove this comment block when implementing this class!
- *
- * CHECKSTYLE:OFF
- *
- * TODO
+ * ATTENTION: Checkstyle turned off! remove this comment block when implementing this
+ * class! CHECKSTYLE:OFF TODO
  */
 
 /**
  * TODO Document this type.
- *
  */
 public class AnalyserBlackboardView {
+
 	public AnalyserBlackboardView(Blackboard blackboard) {
 		// TODO: Implement method
+	}
+
+	public Collection<ResourceDemandingInternalAction> getAllResourceDemandingInternalActions() {
+		return null; // TODO Implement method
+	}
+
+	public Collection<SEFFBranch> getAllSeffBranches() {
+		return null; // TODO Implement method
+	}
+
+	public Collection<SEFFLoop> getAllSeffLoops() {
+		return null; // TODO Implement method
+	}
+
+	public void addAnalyseResult() {
+		// TODO Implement method
+	}
+
+	public Collection<ResourceDemandingInternalAction> getResourceDemandingInternalActionsToBeMeasured() {
+		return null; // TODO Implement method
+	}
+
+	public Collection<SEFFBranch> getSeffBranchesToBeMeasured() {
+		return null; // TODO Implement method
+	}
+
+	public Collection<SEFFLoop> getSeffLoopsToBeMeasured() {
+		return null; // TODO Implement method
 	}
 }
