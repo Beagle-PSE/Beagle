7991d15398154465bd828902022dadde2c5f8f2a
m1cm1c <michelbach94@gmail.com>
2016-01-04 22:55:31 +0100

    Added `readFor` to `FinalJudgeBlackboardView`.

--- src/main/java/de/uka/ipd/sdq/beagle/judge/FinalJudgeBlackboardView.java
+++ src/main/java/de/uka/ipd/sdq/beagle/judge/FinalJudgeBlackboardView.java
@@ -93,6 +93,21 @@ public class FinalJudgeBlackboardView {
 	}
 
 	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#readFor(WRITTEN_TYPE)}.
+	 *
+	 * @param writer The class the desired data was written for. Must not be {@code null}.
+	 * @param <WRITTEN_TYPE> The type of the data to be read.
+	 * @return The data written in the last call to {@linkplain #writeFor} for
+	 *         {@code writer}. {@code null} if no data has been written for {@code writer}
+	 *         yet.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#readFor(WRITTEN_TYPE)
+	 */
+	public <WRITTEN_TYPE extends Serializable> WRITTEN_TYPE readFor(
+		final Class<? extends BlackboardStorer<WRITTEN_TYPE>> writer) {
+		return null;
+	}
+
+	/**
 	 * Delegates to
 	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SEFFLoop)}.
 	 *
