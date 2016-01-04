e3ca226ecf0641faaa998e9dd80c0f166af4bbe6
m1cm1c <michelbach94@gmail.com>
2016-01-04 15:40:43 +0100

    Replaced `addAnalyseResult()` by `proposeExpressionFor()` variants.

--- src/main/java/de/uka/ipd/sdq/beagle/analysis/AnalyserBlackboardView.java
+++ src/main/java/de/uka/ipd/sdq/beagle/analysis/AnalyserBlackboardView.java
@@ -4,6 +4,7 @@ import de.uka.ipd.sdq.beagle.core.Blackboard;
 import de.uka.ipd.sdq.beagle.core.ResourceDemandingInternalAction;
 import de.uka.ipd.sdq.beagle.core.SEFFBranch;
 import de.uka.ipd.sdq.beagle.core.SEFFLoop;
+import de.uka.ipd.sdq.beagle.core.expressions.EvaluableExpression;
 
 import java.util.Set;
 
@@ -60,12 +61,36 @@ public class AnalyserBlackboardView {
 	}
 
 	/**
-	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#addAnalyseResult()}.
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#proposeExpressionFor()}.
 	 *
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#addAnalyseResult()
+	 * @param rdia A resource demanding internal action. Must not be {@code null}.
+	 * @param expression An evaluable expression proposed to describe {@code rdia}’s
+	 *            measurement results. Must not be {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#proposeExpressionFor()
 	 */
-	public void addAnalyseResult() {
+	public void proposeExpressionFor(final ResourceDemandingInternalAction rdia, final EvaluableExpression expression) {
+	}
 
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#proposeExpressionFor()}.
+	 *
+	 * @param loop A SEFF Loop. Must not be {@code null}.
+	 * @param expression An evaluable expression proposed to describe {@code loop}’s
+	 *            measurement results. Must not be {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#proposeExpressionFor()
+	 */
+	public void proposeExpressionFor(final SEFFLoop loop, final EvaluableExpression expression) {
+	}
+
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#proposeExpressionFor()}.
+	 *
+	 * @param branch A SEFF Branch. Must not be {@code null}.
+	 * @param expression An evaluable expression proposed to describe {@code branch}’s
+	 *            measurement results. Must not be {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#proposeExpressionFor()
+	 */
+	public void proposeExpressionFor(final SEFFBranch branch, final EvaluableExpression expression) {
 	}
 
 	/**
