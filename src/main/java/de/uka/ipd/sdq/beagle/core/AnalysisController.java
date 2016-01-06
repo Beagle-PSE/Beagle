b5e660bc39b8c7f73f324a101f033828064b2d5d
Joshua Gleitze <git@joshuagleitze.de>
2016-01-06 20:15:30 +0100

    AnalysisController: fix use of old Blackboard constructor.

--- src/main/java/de/uka/ipd/sdq/beagle/core/AnalysisController.java
+++ src/main/java/de/uka/ipd/sdq/beagle/core/AnalysisController.java
@@ -49,7 +49,7 @@ public class AnalysisController {
 	 * data exchange in any action invoked by this controller must happen exclusively via
 	 * this {@link Blackboard} instance.
 	 */
-	private final Blackboard blackboard = new Blackboard();
+	private final Blackboard blackboard = null;
 
 	/**
 	 * Creates a controller to analyse all elements written on {@code blackboard}.
