3e51b5fcebd13e52d9a7ed749478cde8c20c0137
m1cm1c <michelbach94@gmail.com>
2016-01-09 12:27:03 +0100

    Wrote Javadoc for `UserConfiguration` and `BeagleAnalysisWizard`.

--- Core/src/main/java/de/uka/ipd/sdq/beagle/gui/BeagleAnalysisWizard.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/gui/BeagleAnalysisWizard.java
@@ -1,6 +1,11 @@
 package de.uka.ipd.sdq.beagle.gui;
 
-
+/**
+ * Sets up and controls the graphical wizard allowing the user to configure Beagle’s
+ * behaviour during the analysis and afterwards starts the analysis.
+ * 
+ * @author Christoph Michelbach
+ */
 public class BeagleAnalysisWizard {
 
 }
--- Core/src/main/java/de/uka/ipd/sdq/beagle/gui/UserConfiguration.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/gui/UserConfiguration.java
@@ -1,6 +1,10 @@
 package de.uka.ipd.sdq.beagle.gui;
 
-
+/**
+ * Stores the configuration of the analysis by the user.
+ * 
+ * @author Christoph Michelbach
+ */
 public class UserConfiguration {
 
 }
