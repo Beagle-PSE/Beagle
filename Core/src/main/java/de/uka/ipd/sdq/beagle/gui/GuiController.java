ef274a9569ae934e8edcc4756cd44791e9cfcbab
m1cm1c <michelbach94@gmail.com>
2016-01-09 12:39:07 +0100

    Wrote Javadoc for `GuiControler`.

--- Core/src/main/java/de/uka/ipd/sdq/beagle/gui/GuiController.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/gui/GuiController.java
@@ -2,12 +2,30 @@ package de.uka.ipd.sdq.beagle.gui;
 
 import java.util.Set;
 
+/**
+ * Controls the Graphical User Interface (GUI). One {@code GuiController} corresponds to
+ * exactly one instance of the GUI. Opening it several times is not possible. Note that
+ * it's possible for a GUI instance to exist while not being open.
+ * 
+ * @author Christoph Michelbach
+ */
 public class GuiController {
 
-	public GuiController(Set<Object> components) { // refine type
-
+	/**
+	 * Creates a new {@link GuiController} with the given components. It's not possible to
+	 * change the set of components but the user can choose that certain components will
+	 * not be measured even though they are in the set {@code components}.
+	 *
+	 * @param components The components this {@link GuiController} will use.
+	 */
+	public GuiController(final Set<Object> components) {
+		// refine type
 	}
 
+	/**
+	 * Opens the GuiController.
+	 *
+	 */
 	public void open() {
 
 	}
