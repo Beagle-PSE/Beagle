7e7ece86691799320c2bc4c27b02016d98b28cda
m1cm1c <michelbach94@gmail.com>
2016-01-09 12:03:09 +0100

    Added class `BeagleController`.

---
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/BeagleController.java
@@ -0,0 +1,26 @@
+package de.uka.ipd.sdq.beagle.core;
+
+import de.uka.ipd.sdq.beagle.gui.UserConfiguration;
+
+public class BeagleController {
+
+	public BeagleController(UserConfiguration userConfiguration) {
+
+	}
+
+	public void startAnalysis() {
+
+	}
+
+	public void pauseAnalysis() {
+
+	}
+
+	public void continueAnalysis() {
+
+	}
+
+	public void abortAnalysis() {
+
+	}
+}
