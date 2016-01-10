a2e338b111e14d6555a9438895a6bf2972237767
m1cm1c <michelbach94@gmail.com>
2016-01-10 18:00:52 +0100

    Ordered methods.

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/MeasurementControllerBlackboardView.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/MeasurementControllerBlackboardView.java
@@ -19,112 +19,6 @@ import java.util.Set;
 public class MeasurementControllerBlackboardView {
 
 	/**
-	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllRdias()}.
-	 *
-	 * @return all {@linkplain ResourceDemandingInternalAction resource demanding internal
-	 *         actions} known to Beagle. Changes to the returned set will not modify the
-	 *         blackboard content. Is never {@code null}.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getAllRdias()
-	 */
-	public Set<ResourceDemandingInternalAction> getAllRDIAs() {
-		return null;
-	}
-
-	/**
-	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffBranches()}.
-	 *
-	 * @return all {@linkplain SeffBranch SEFF branches} known to Beagle. Changes to the
-	 *         returned set will not modify the blackboard content. Is never {@code null}.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffBranches()
-	 */
-	public Set<SeffBranch> getAllSEFFBranches() {
-		return null;
-	}
-
-	/**
-	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffLoops()}.
-	 *
-	 * @return all {@linkplain SeffLoop SEFF loops} known to Beagle. Changes to the
-	 *         returned set will not modify the blackboard content. Is never {@code null}.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffLoops()
-	 */
-	public Set<SeffLoop> getAllSeffLoops() {
-		return null;
-	}
-
-	/**
-	 * Delegates to
-	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#writeFor(Class, Serializable)} .
-	 *
-	 * @param writer The class the data should be written for. Must not be {@code null}.
-	 * @param written The data to write.
-	 * @param <WRITTEN_TYPE> {@code written}’s type.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#writeFor(Class, Serializable)
-	 */
-	public <WRITTEN_TYPE extends Serializable> void writeFor(
-		final Class<? extends BlackboardStorer<WRITTEN_TYPE>> writer, final WRITTEN_TYPE written) {
-	}
-
-	/**
-	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#readFor(Class)} .
-	 *
-	 * @param writer The class the desired data was written for. Must not be {@code null}.
-	 * @param <WRITTEN_TYPE> The type of the data to be read.
-	 * @return The data written in the last call to
-	 *         {@linkplain de.uka.ipd.sdq.beagle.core.Blackboard#writeFor(Class, Serializable)}
-	 *         for {@code writer}. {@code null} if no data has been written for
-	 *         {@code writer} yet.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#readFor(Class)
-	 */
-	public <WRITTEN_TYPE extends Serializable> WRITTEN_TYPE readFor(
-		final Class<? extends BlackboardStorer<WRITTEN_TYPE>> writer) {
-		return null;
-	}
-
-	/**
-	 * Delegates to
-	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffLoop)}.
-	 *
-	 * @param loop A SEFF Loop to get the measurement results of. Must not be {@code null}
-	 *            .
-	 * @return All measurement results reported for {@code loop}. Changes to the returned
-	 *         set will not modify the blackboard content. Is never {@code null}.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffLoop)
-	 */
-	public Set<ResourceDemandMeasurementResult> getMeasurementResultsFor(final SeffLoop loop) {
-		return null;
-	}
-
-	/**
-	 * Delegates to
-	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffBranch)}.
-	 *
-	 * @param branch A SEFF Branch to get the measurement results of. Must not be
-	 *            {@code null}.
-	 * @return All measurement results reported for {@code branch}. Changes to the
-	 *         returned set will not modify the blackboard content. Is never {@code null}.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffBranch)
-	 */
-	public Set<BranchDecisionMeasurementResult> getMeasurementResultsFor(final SeffBranch branch) {
-		return null;
-	}
-
-	/**
-	 * Delegates to
-	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(ResourceDemandingInternalAction)}
-	 * .
-	 *
-	 * @param rdia An resource demanding internal action to get the measurement results
-	 *            of. Must not be {@code null}.
-	 * @return All measurement results reported for {@code rdia}. Changes to the returned
-	 *         set will not modify the blackboard content. Is never {@code null}.
-	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(ResourceDemandingInternalAction)
-	 */
-	public Set<ResourceDemandMeasurementResult> getMeasurementResultsFor(final ResourceDemandingInternalAction rdia) {
-		return null;
-	}
-
-	/**
 	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getRdiasToBeMeasured()}.
 	 *
 	 * @return All {@linkplain ResourceDemandingInternalAction resource demanding internal
@@ -132,7 +26,7 @@ public class MeasurementControllerBlackboardView {
 	 *         blackboard content. Is never {@code null}.
 	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getRdiasToBeMeasured()
 	 */
-	public Set<ResourceDemandingInternalAction> getRDIAsToBeMeasured() {
+	public Set<ResourceDemandingInternalAction> getRdiasToBeMeasured() {
 		return null;
 	}
 
@@ -144,7 +38,7 @@ public class MeasurementControllerBlackboardView {
 	 *         returned set will not modify the blackboard content. Is never {@code null}.
 	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getSeffBranchesToBeMeasured()
 	 */
-	public Set<SeffBranch> getSEFFBranchesToBeMeasured() {
+	public Set<SeffBranch> getSeffBranchesToBeMeasured() {
 		return null;
 	}
 
@@ -195,5 +89,111 @@ public class MeasurementControllerBlackboardView {
 	 */
 	public void reportMeasurementResultForSeffBranch(final SeffBranch branch,
 		final BranchDecisionMeasurementResult result) {
+	}
+
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllRdias()}.
+	 *
+	 * @return all {@linkplain ResourceDemandingInternalAction resource demanding internal
+	 *         actions} known to Beagle. Changes to the returned set will not modify the
+	 *         blackboard content. Is never {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getAllRdias()
+	 */
+	public Set<ResourceDemandingInternalAction> getAllRDIAs() {
+		return null;
+	}
+
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffBranches()}.
+	 *
+	 * @return all {@linkplain SeffBranch SEFF branches} known to Beagle. Changes to the
+	 *         returned set will not modify the blackboard content. Is never {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffBranches()
+	 */
+	public Set<SeffBranch> getAllSEFFBranches() {
+		return null;
+	}
+
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffLoops()}.
+	 *
+	 * @return all {@linkplain SeffLoop SEFF loops} known to Beagle. Changes to the
+	 *         returned set will not modify the blackboard content. Is never {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getAllSeffLoops()
+	 */
+	public Set<SeffLoop> getAllSeffLoops() {
+		return null;
+	}
+
+	/**
+	 * Delegates to
+	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffLoop)}.
+	 *
+	 * @param loop A SEFF Loop to get the measurement results of. Must not be {@code null}
+	 *            .
+	 * @return All measurement results reported for {@code loop}. Changes to the returned
+	 *         set will not modify the blackboard content. Is never {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffLoop)
+	 */
+	public Set<ResourceDemandMeasurementResult> getMeasurementResultsFor(final SeffLoop loop) {
+		return null;
+	}
+
+	/**
+	 * Delegates to
+	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffBranch)}.
+	 *
+	 * @param branch A SEFF Branch to get the measurement results of. Must not be
+	 *            {@code null}.
+	 * @return All measurement results reported for {@code branch}. Changes to the
+	 *         returned set will not modify the blackboard content. Is never {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(SeffBranch)
+	 */
+	public Set<BranchDecisionMeasurementResult> getMeasurementResultsFor(final SeffBranch branch) {
+		return null;
+	}
+
+	/**
+	 * Delegates to
+	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(ResourceDemandingInternalAction)}
+	 * .
+	 *
+	 * @param rdia An resource demanding internal action to get the measurement results
+	 *            of. Must not be {@code null}.
+	 * @return All measurement results reported for {@code rdia}. Changes to the returned
+	 *         set will not modify the blackboard content. Is never {@code null}.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#getMeasurementResultsFor(ResourceDemandingInternalAction)
+	 */
+	public Set<ResourceDemandMeasurementResult> getMeasurementResultsFor(final ResourceDemandingInternalAction rdia) {
+		return null;
+	}
+
+	/**
+	 * Delegates to {@link de.uka.ipd.sdq.beagle.core.Blackboard#readFor(Class)} .
+	 *
+	 * @param writer The class the desired data was written for. Must not be {@code null}.
+	 * @param <WRITTEN_TYPE> The type of the data to be read.
+	 * @return The data written in the last call to
+	 *         {@linkplain de.uka.ipd.sdq.beagle.core.Blackboard#writeFor(Class, Serializable)}
+	 *         for {@code writer}. {@code null} if no data has been written for
+	 *         {@code writer} yet.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#readFor(Class)
+	 */
+	public <WRITTEN_TYPE extends Serializable> WRITTEN_TYPE readFor(
+		final Class<? extends BlackboardStorer<WRITTEN_TYPE>> writer) {
+		return null;
+	}
+
+	/**
+	 * Delegates to
+	 * {@link de.uka.ipd.sdq.beagle.core.Blackboard#writeFor(Class, Serializable)} .
+	 *
+	 * @param writer The class the data should be written for. Must not be {@code null}.
+	 * @param written The data to write.
+	 * @param <WRITTEN_TYPE> {@code written}’s type.
+	 * @see de.uka.ipd.sdq.beagle.core.Blackboard#writeFor(Class, Serializable)
+	 */
+	public <WRITTEN_TYPE extends Serializable> void writeFor(
+		final Class<? extends BlackboardStorer<WRITTEN_TYPE>> writer, final WRITTEN_TYPE written) {
 	}
 }
