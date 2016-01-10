f55641567d4d794f241582767ab91cdd6c84ef3d
m1cm1c <michelbach94@gmail.com>
2016-01-10 16:32:41 +0100

    Introduced class `EvaluableExpressionFitnessFunction`.

---
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/EvaluableExpressionFitnessFunction.java
@@ -0,0 +1,13 @@
+package de.uka.ipd.sdq.beagle.core;
+
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.EvaluableExpression;
+
+/**
+ * Holds and is responsible allows access to the fitness function grading
+ * {@linkplain EvaluableExpression evaluable expressions} regarding their fitness.
+ * 
+ * @author Christoph Michelbach
+ */
+public class EvaluableExpressionFitnessFunction {
+
+}
