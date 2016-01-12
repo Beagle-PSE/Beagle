2179db3bb37224280061b6ef1565089072198be5
m1cm1c <michelbach94@gmail.com>
2016-01-12 23:45:27 +0100

    Imported context menus prototype.

---
+++ GUI/src/de/uka/ipd/sdq/beagle/prototypes/contextmenus/handlers/DemoBeagleContextMenuEntryHandlerForRepositories.java
@@ -0,0 +1,56 @@
+package de.uka.ipd.sdq.beagle.prototypes.contextmenus.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+/**
+ * Handles the context menu entries, that start an analysis of the whole project.
+ *
+ * @author Roman Langrehr
+ */
+public class DemoBeagleContextMenuEntryHandlerForRepositories extends AbstractHandler {
+
+	/**
+	 * RegEx for the file extensions, where the context menu entry should be displayed.
+	 */
+	private static final String FILE_EXTENSION_MATCHER = "repository|repository_diagram";
+
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		final Shell shell = HandlerUtil.getActiveShell(event);
+		final ISelection selection = HandlerUtil.getActiveMenuSelection(event);
+		final IStructuredSelection structuredSelection = (IStructuredSelection) selection;
+
+		final Object firstElement = structuredSelection.getFirstElement();
+		if (firstElement instanceof IFile) {
+			final IFile clickedFile = (IFile) firstElement;
+			final IPath clickedFilePath = clickedFile.getFullPath();
+			if (clickedFilePath.getFileExtension().matches(FILE_EXTENSION_MATCHER)) {
+				final IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+				MessageDialog.openInformation(window.getShell(), "Beagle is alive!",
+						"Belive it, or not. But Beagle ist alive!\n" + "You want to analyse: The whole repository: "
+								+ clickedFilePath.toString());
+			} else {
+				MessageDialog.openInformation(shell, "Error",
+						"The handler was not executet with a file with a proper file extension.\n"
+								+ "This should not be possible!\n" + "selection.getFirstElement() returned a path to: "
+								+ clickedFilePath.toString());
+			}
+		} else {
+			MessageDialog.openInformation(shell, "Error",
+					"The handler was not executet with a file.\n" + "This should not be possible!\n"
+							+ "selection.getFirstElement() returned " + firstElement.toString());
+		}
+		return null;
+	}
+
+}
