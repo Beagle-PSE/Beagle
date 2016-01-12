0cacf15279e979326d9d4154ab5ec8b6ce406465
m1cm1c <michelbach94@gmail.com>
2016-01-13 00:35:03 +0100

    Introduced `BeagleAnalysisWizard`.

---
+++ GUI/src/de/uka/ipd/sdq/beagle/gui/BeagleAnalysisWizard.java
@@ -0,0 +1,11 @@
+package de.uka.ipd.sdq.beagle.gui;
+
+/**
+ * The class setting up and controlling the wizard allowing the user to configure Beagle’s
+ * behaviour during the analysis.
+ * 
+ * @author Christoph Michelbach
+ */
+public class BeagleAnalysisWizard {
+
+}
