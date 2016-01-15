36e3a6b78466dcbe0f932f009a6a86a0ddb61702
Annika Berger <annika.c.berger@gmail.com>
2016-01-15 17:39:55 +0100

    added Test File for Code Section

---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/CodeSectionTest.java
@@ -0,0 +1,35 @@
+package de.uka.ipd.sdq.beagle.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class CodeSectionTest {
+
+	@Test
+	public void testCodeSection() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStartFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStartCodeLine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEndCodeLine() {
+		fail("Not yet implemented");
+	}
+
+}
