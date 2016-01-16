95741062381cd01060b2f1fde0f35a8d7790f1b5
m1cm1c <michelbach94@gmail.com>
2016-01-16 18:25:05 +0100

    Moved prototype (2).

---
+++ Prototypes/Eclipse GUI with Wizards and Dialogs/src/main/java/de/uka/ipd/sdq/beagle/prototypes/gui/BeagleAnalysisWizard.java
@@ -0,0 +1,69 @@
+package de.uka.ipd.sdq.beagle.prototypes.gui;
+
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * The class setting up and controlling the wizard allowing the user to configure Beagle’s
+ * behaviour during the analysis and afterwards starting the analysis.
+ * 
+ * @author Christoph Michelbach
+ */
+public class BeagleAnalysisWizard extends Wizard {
+
+	/**
+	 * A page of this wizard.
+	 */
+	private SelectionOverviewPage selectionOverviewPage;
+
+	/**
+	 * A page of this wizard.
+	 */
+	private TimeoutPage timeoutPage;
+
+	/**
+	 * The constructor of this class.
+	 */
+	public BeagleAnalysisWizard() {
+		super();
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public String getWindowTitle() {
+		return "Beagle Analysis";
+	}
+
+	@Override
+	public void addPages() {
+		this.selectionOverviewPage = new SelectionOverviewPage();
+		this.timeoutPage = new TimeoutPage();
+
+		addPage(this.selectionOverviewPage);
+		addPage(this.timeoutPage);
+	}
+
+	@Override
+	public boolean performFinish() {
+		// open dialog
+		// TODO use timeout.getTimeout() when launching the analysis
+		final String dialogTitle = "Beagle Analysis is Running";
+		final String dialogMessage = "Beagle Analysis is running.";
+		final String[] buttonLabels = {"Abort", "Pause"};
+		final MessageDialog dialog = new MessageDialog(new Shell(), dialogTitle, null, dialogMessage,
+			MessageDialog.INFORMATION, buttonLabels, 0);
+		final int buttonClick = dialog.open();
+
+		if (buttonClick == 0) {
+			System.out.println("User clicked 'Abort'.");
+		}
+
+		if (buttonClick == 1) {
+			System.out.println("User clicked 'Pause'.");
+		}
+
+		return true;
+	}
+
+}
