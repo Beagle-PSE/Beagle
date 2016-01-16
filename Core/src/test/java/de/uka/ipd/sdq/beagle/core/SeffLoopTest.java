7e4deda8df340f10a8e97705b5da06eb0f2bd0c7
Annika Berger <annika.c.berger@gmail.com>
2016-01-16 10:20:09 +0100

    testfiles

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/SeffLoopTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/SeffLoopTest.java
@@ -1,8 +1,12 @@
 package de.uka.ipd.sdq.beagle.core;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.theInstance;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import java.io.File;
 
 /**
  * Tests {@link de.uka.ipd.sdq.beage.core.SeffLoop} and contains all test cases needed to
@@ -18,8 +22,19 @@ public class SeffLoopTest {
 	 * .
 	 */
 	@Test
-	public void testSeffLoop() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		final File file =
+			new File(SeffLoopTest.class.getResource("de.uka.ipd.sdq.beagle.core.TestFile.java").getPath());
+		final int startCodeLine = 4;
+		final int endCodeLine = 15;
+		final CodeSection[] codeSections = {null, new CodeSection(file, startCodeLine, file, endCodeLine)
+
+		};
+		SeffLoop loop;
+		for (CodeSection codeSection : codeSections) {
+			loop = new SeffLoop(codeSection);
+			assertThat(loop.getLoopBody(), is(theInstance(codeSection)));
+		}
 	}
 
 	/**
@@ -27,7 +42,23 @@ public class SeffLoopTest {
 	 */
 	@Test
 	public void testGetLoopBody() {
-		fail("Not yet implemented");
+		final File file =
+			new File(SeffLoopTest.class.getResource("de.uka.ipd.sdq.beagle.core.TestFile.java").getPath());
+		final int startCodeLine = 4;
+		final int endCodeLine = 15;
+		final File file2 =
+			new File(SeffLoopTest.class.getResource("de.uka.ipd.sdq.beagle.core.PingPong.java").getPath());
+		final int startCodeLine2 = 60;
+		final int endCodeLine2 = 68;
+		final CodeSection[] codeSections = {new CodeSection(file, startCodeLine, file, endCodeLine),
+			new CodeSection(file2, startCodeLine2, file2, endCodeLine2)
+
+		};
+		SeffLoop loop;
+		for (CodeSection codeSection : codeSections) {
+			loop = new SeffLoop(codeSection);
+			assertThat(loop.getLoopBody(), is(theInstance(codeSection)));
+		}
 	}
 
 }
