3137ce28b1ba760c9b478b40e49c0edf72b5c1cc
Roman Langrehr <roman.langrehr@yahoo.de>
2016-01-16 15:00:37 +0100

    Removed prototyps to put them back in the correct directory.

--- prototypes/Eclipse Extension Point Prototype/src/main/java/de/uka/ipd/sdq/beagle/prototypes/extensionpoint/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This prototype adds an extension point for measurement tools and scans for measurement
- * tools.
- *
- * @author Roman Langrehr
- */
-package de.uka.ipd.sdq.beagle.prototypes.extensionpoint;
--- prototypes/Measurement Tool Mockup/src/main/java/de/uka/ipd/sdq/beagle/prototypes/extensionpoint/measurementtoolmockup/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This mockup contains an (empty) implementation of
- * {@link de.uka.sdq.beagle.measurement.MeasurementTool}.
- *
- * @author Roman Langrehr
- */
-package de.uka.ipd.sdq.beagle.prototypes.extensionpoint.measurementtoolmockup;
