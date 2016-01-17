4f03e109ecd659fe08af4806464b5761f6b6e859
m1cm1c <michelbach94@gmail.com>
2016-01-17 21:10:47 +0100

    Conflict solving part 3 of 3.

--- GUI/src/main/java/de/uka/ipd/sdq/beagle/gui/BeagleAnalysisWizard.java
+++ GUI/src/main/java/de/uka/ipd/sdq/beagle/gui/BeagleAnalysisWizard.java
@@ -1,24 +1,70 @@
 package de.uka.ipd.sdq.beagle.gui;
 
+import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.jface.wizard.WizardPage;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
- * Sets up and controls the graphical wizard allowing the user to configure Beagle’s
- * behaviour during the analysis and afterwards starts the analysis.
- *
+ * The class setting up and controlling the wizard allowing the user to configure Beagle’s
+ * behaviour during the analysis.
+ * 
  * @author Christoph Michelbach
  */
-public class BeagleAnalysisWizard {
+public class BeagleAnalysisWizard extends Wizard {
 
 	/**
-	 * The user configuration associated with this {@link BeagleAnalysisWizard}.
+	 * The {@link UserConfiguration} this {@link BeagleAnalysisWizard} and therefore
+	 * everything linked to it uses.
 	 */
-	@SuppressWarnings("unused")
-	private UserConfiguration userConfiguration;
+	private final UserConfiguration userConfiguration;
 
 	/**
-	 * The {@link WizardPage}s of this {@link BeagleAnalysisWizard}.
+	 * The {@link ActionListener} which will be executed when the wizard finishes.
 	 */
-	@SuppressWarnings("unused")
-	private WizardPage[] wizardPages;
+	private ActionListener wizardFinished;
+
+	/**
+	 * Constructs a new {@link BeagleAnalysisWizard} which runs {@code wizardFinished}
+	 * when it finishes.
+	 * 
+	 * @param userConfiguration The {@link UserConfiguration} this
+	 *            {@link BeagleAnalysisWizard} and therefore everything linked to it will
+	 *            use.
+	 * @param wizardFinished The {@link ActionListener} which will be executed when the
+	 *            wizard finishes.
+	 */
+	public BeagleAnalysisWizard(final UserConfiguration userConfiguration, final ActionListener wizardFinished) {
+		super();
+		this.userConfiguration = userConfiguration;
+		this.wizardFinished = wizardFinished;
+		setNeedsProgressMonitor(true);
+	}
+
+	@Override
+	public String getWindowTitle() {
+		return "Beagle Analysis";
+	}
+
+	@Override
+	public void addPages() {
+		final List<WizardPage> wizardPages = new LinkedList<WizardPage>();
+		wizardPages.add(new SelectionOverviewWizardPage(userConfiguration));
+		wizardPages.add(new TimeoutWizardPage(userConfiguration));
+		final Iterator<WizardPage> iterator = wizardPages.iterator();
+
+		while (iterator.hasNext()) {
+			addPage(iterator.next());
+		}
+	}
+
+	@Override
+	public boolean performFinish() {
+		this.wizardFinished.actionPerformed(new ActionEvent(this, 0, "wizard finished"));
+		return true;
+	}
 }
--- GUI/src/main/java/de/uka/ipd/sdq/beagle/gui/package-info.java
+++ GUI/src/main/java/de/uka/ipd/sdq/beagle/gui/package-info.java
@@ -1,4 +1,6 @@
 /**
- * This package contains all files concerning the Graphical User Interface (GUI).
+ * This prototype is an eclipse plugin, that adds the context menus for beagle to eclipse.
+ *
+ * @author Roman Langrehr
  */
 package de.uka.ipd.sdq.beagle.gui;
