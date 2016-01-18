bb6ceaee0d8f5fb9cfc2a4feeaf9c790504fc5cb
Joshua Gleitze <git@joshuagleitze.de>
2016-01-18 22:20:14 +0100

    Added test class for Blackboard

---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/BlackboardTest.java
@@ -0,0 +1,169 @@
+package de.uka.ipd.sdq.beagle.core;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class BlackboardTest {
+
+	@Test
+	public void testBlackboard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllRdias() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllSeffBranches() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllSeffLoops() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAllExternalCallParameters() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRdiasToBeMeasured() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSeffBranchesToBeMeasured() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSeffLoopsToBeMeasured() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetExternalCallParametersToBeMeasured() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredRdiasResourceDemandingInternalActionArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredRdiasCollectionOfResourceDemandingInternalAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredSeffBranchesSeffBranchArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredSeffBranchesCollectionOfSeffBranch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredSeffLoopsSeffLoopArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredSeffLoopsCollectionOfSeffLoop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredExternalCallParametersExternalCallParameterArray() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToBeMeasuredExternalCallParametersCollectionOfExternalCallParameter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMeasurementResultsForResourceDemandingInternalAction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMeasurementResultsForSeffBranch() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMeasurementResultsForSeffLoop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMeasurementResultsForExternalCallParameter() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeasurementResultForResourceDemandingInternalActionResourceDemandMeasurementResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeasurementResultForSeffBranchBranchDecisionMeasurementResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeasurementResultForSeffLoopLoopRepetitionCountMeasurementResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddMeasurementResultForExternalCallParameterParameterChangeMeasurementResult() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetProposedExpressionFor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddProposedExpressionFor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFinalExpressionFor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetFinalExpressionFor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetFitnessFunction() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testWriteFor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testReadFor() {
+		fail("Not yet implemented");
+	}
+
+}
