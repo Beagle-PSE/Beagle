2bd2ab36fbc313e6e3a0b7747b96fedd33b514ed
Annika Berger <annika.c.berger@gmail.com>
2016-01-27 16:22:23 +0100

    added Stub for ParameterisationDependentMeasurementResultTest and adapted the other tests by removing the methods which will be tested in the newly created Test Class.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/BranchDecisionMeasurementResultTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/BranchDecisionMeasurementResultTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.fail;
 import org.junit.Test;
 
 /**
- * Tests {@link LoopRepetitionCountMeasurementResult} and contains all test cases needed
+ * Tests {@link BranchDecisionMeasurementResult} and contains all test cases needed
  * to check every method.
  * 
  * @author Annika Berger
@@ -36,53 +36,6 @@ public class BranchDecisionMeasurementResultTest {
 	 */
 	@Test
 	public void testGetBranchIndex() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link ParameterisationDependentMeasurementResult#ParameterisationDependentMeasurementResult()}
-	 * .
-	 */
-	@Test
-	public void testParameterisationDependentMeasurementResult() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link ParameterisationDependentMeasurementResult#ParameterisationDependentMeasurementResult(Parameterisation)}
-	 * .
-	 */
-	@Test
-	public void testParameterisationDependentMeasurementResultParameterisation() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link ParameterisationDependentMeasurementResult#getParameterisation()}.
-	 */
-	@Test
-	public void testGetParameterisation() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for
-	 * {@link ParameterisationDependentMeasurementResult#equals(java.lang.Object)} and
-	 * {@link ParameterisationDependentMeasurementResult#hashCode()}.
-	 */
-	@Test
-	public void testEqualsObject() {
-		fail("Not yet implemented");
-	}
-
-	/**
-	 * Test method for {@link ParameterisationDependentMeasurementResult#toString()}.
-	 */
-	@Test
-	public void testToString() {
 		fail("Not yet implemented");
 	}
 
---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/ParameterisationDependentMeasurementResultTest.java
@@ -0,0 +1,63 @@
+package de.uka.ipd.sdq.beagle.core.measurement;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link ParameterisationDependentMeasurementResult} and contains all test cases needed
+ * to check every method.
+ * 
+ * @author Annika Berger
+ */
+public class ParameterisationDependentMeasurementResultTest {
+
+
+	/**
+	 * Test method for
+	 * {@link ParameterisationDependentMeasurementResult#ParameterisationDependentMeasurementResult()}
+	 * .
+	 */
+	@Test
+	public void testParameterisationDependentMeasurementResult() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link ParameterisationDependentMeasurementResult#ParameterisationDependentMeasurementResult(Parameterisation)}
+	 * .
+	 */
+	@Test
+	public void testParameterisationDependentMeasurementResultParameterisation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link ParameterisationDependentMeasurementResult#getParameterisation()} .
+	 */
+	@Test
+	public void testGetParameterisation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link ParameterisationDependentMeasurementResult#equals(java.lang.Object)} and
+	 * {@link ParameterisationDependentMeasurementResult#hashCode()}.
+	 */
+	@Test
+	public void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link ParameterisationDependentMeasurementResult#toString()} .
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
