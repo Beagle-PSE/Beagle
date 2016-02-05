f5b5fa10cf8e601c2e9bc8469059033862eda12d
Annika Berger <annika.c.berger@gmail.com>
2016-02-05 13:53:02 +0100

    Adapted dependencies for LaunchConfiguration

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/measurement/MeasurementEventParser.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/measurement/MeasurementEventParser.java
@@ -12,8 +12,8 @@ import de.uka.ipd.sdq.beagle.core.measurement.order.MeasurementEventVisitor;
 import de.uka.ipd.sdq.beagle.core.measurement.order.ParameterValueCapturedEvent;
 import de.uka.ipd.sdq.beagle.core.measurement.order.ResourceDemandCapturedEvent;
 
-import org.apache.commons.lang3.Validate;
 import org.apache.commons.collections4.IteratorUtils;
+import org.apache.commons.lang3.Validate;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -39,7 +39,7 @@ public class MeasurementEventParser {
 	/**
 	 * All measurement events in chronological order.
 	 */
-	private List<MeasurementEvent> measurementEvents;
+	private final List<MeasurementEvent> measurementEvents;
 
 	/**
 	 * Maps for each code sections to all occurrences of measurement events for this code
@@ -69,7 +69,7 @@ public class MeasurementEventParser {
 	 */
 	public MeasurementEventParser(final Iterable<MeasurementEvent> events) {
 		Validate.noNullElements(events);
-		this.measurementEvents = IteratorUtils.toList(events);
+		this.measurementEvents = IteratorUtils.toList(events.iterator());
 		this.createCodeSectionMapping();
 	}
 
@@ -175,12 +175,12 @@ public class MeasurementEventParser {
 		 * The {@link ResourceDemandingInternalAction} where we want to know the new
 		 * measurement results.
 		 */
-		private ResourceDemandingInternalAction resourceDemandingInternalAction;
+		private final ResourceDemandingInternalAction resourceDemandingInternalAction;
 
 		/**
 		 * The set, where the {@link ResourceDemandMeasurementResult} should be added.
 		 */
-		private Set<ResourceDemandMeasurementResult> resourceDemandMeasurementResults;
+		private final Set<ResourceDemandMeasurementResult> resourceDemandMeasurementResults;
 
 		/**
 		 * Creates a visitor for a specific {@link ResourceDemandingInternalAction}.
@@ -213,55 +213,6 @@ public class MeasurementEventParser {
 						.add(new ResourceDemandMeasurementResult(resourceDemandCapturedEvent.getValue()));
 				}
 			}
-		}
-
-		@Override
-		public void visit(final ParameterValueCapturedEvent parameterValueCapturedEvent) {
-			// We don't care about them.
-		}
-	}
-
-	/**
-	 * A {@link MeasurementEventVisitor} for a specific {@link SeffBranch}.
-	 *
-	 * <p>Only {@linkplain MeasurementEvent MeasurementEvents} with the correct
-	 * {@link CodeSection} may be visited with this visitor.
-	 *
-	 * @author Roman Langrehr
-	 */
-	private class SeffBranchMeasurementEventVisitor implements MeasurementEventVisitor {
-
-		/**
-		 * The {@link SeffBranch} where we want to know the new measurement results.
-		 */
-		private SeffBranch seffBranch;
-
-		/**
-		 * The set, where the {@link BranchDecisionMeasurementResult} should be added.
-		 */
-		private Set<BranchDecisionMeasurementResult> branchDecisionMeasurementResults;
-
-		/**
-		 * Creates a visitor for a specific {@link SeffBranch}.
-		 *
-		 * @param seffBranch The {@link ResourceDemandingInternalAction} where we want to
-		 *            know the new measurement results.
-		 * @param branchDecisionMeasurementResults The set, where the
-		 *            {@link ResourceDemandMeasurementResult} should be added.
-		 */
-		SeffBranchMeasurementEventVisitor(final SeffBranch seffBranch,
-			final Set<BranchDecisionMeasurementResult> branchDecisionMeasurementResults) {
-			this.seffBranch = seffBranch;
-			this.branchDecisionMeasurementResults = branchDecisionMeasurementResults;
-		}
-
-		@Override
-		public void visit(final CodeSectionExecutedEvent codeSectionExecutedEvent) {
-		}
-
-		@Override
-		public void visit(final ResourceDemandCapturedEvent resourceDemandCapturedEvent) {
-			// We don't care about them.
 		}
 
 		@Override
