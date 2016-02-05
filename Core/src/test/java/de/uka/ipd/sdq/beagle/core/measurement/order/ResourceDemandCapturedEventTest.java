c5fc7e9011c9cdf6ddda20bac8901d9d4bee1710
Annika Berger <annika.c.berger@gmail.com>
2016-02-05 14:22:35 +0100

    implemented all test methods for ResourceDemandCapturedEvent

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/ResourceDemandCapturedEventTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/ResourceDemandCapturedEventTest.java
@@ -1,6 +1,16 @@
 package de.uka.ipd.sdq.beagle.core.measurement.order;
 
-import static org.junit.Assert.fail;
+import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+
+import de.uka.ipd.sdq.beagle.core.CodeSection;
+import de.uka.ipd.sdq.beagle.core.ResourceDemandType;
+import de.uka.ipd.sdq.beagle.core.testutil.factories.CodeSectionFactory;
 
 import org.junit.Test;
 
@@ -13,38 +23,93 @@ import org.junit.Test;
 public class ResourceDemandCapturedEventTest {
 
 	/**
+	 * A {@link CodeSectionFactory} providing methods to get code sections to use for
+	 * tests.
+	 */
+	private static final CodeSectionFactory CODE_SECTION_FACTORY = new CodeSectionFactory();
+
+	/**
 	 * Test method for
 	 * {@link ResourceDemandCapturedEvent#ResourceDemandCapturedEvent(CodeSection, ResourceDemandType, double)}
 	 * .
+	 * 
+	 * <p>Asserts that {@link NullPointerException}s are thrown if one of the arguments is
+	 * null and that an {@link IllegalArgumentException} is thrown if the value is
+	 * negative.
 	 */
 	@Test
-	public void testResourceDemandCapturedEvent() {
-		fail("Not yet implemented");
+	public void resourceDemandCapturedEvent() {
+		final CodeSection codeSection = CODE_SECTION_FACTORY.getOne();
+		final ResourceDemandType type = new ResourceDemandType("Test", true);
+		final Double value = 2.0;
+		new ResourceDemandCapturedEvent(codeSection, type, value);
+
+		final CodeSection codeSection2 = CODE_SECTION_FACTORY.getOne();
+		final ResourceDemandType type2 = new ResourceDemandType("New Type", false);
+		final Double value2 = 0.0;
+		new ResourceDemandCapturedEvent(codeSection2, type2, value2);
+
+		assertThat("Code section must not be null", () -> new ResourceDemandCapturedEvent(null, type, value),
+			throwsException(NullPointerException.class));
+
+		assertThat("Resource demand type must not be null",
+			() -> new ResourceDemandCapturedEvent(codeSection, null, value),
+			throwsException(NullPointerException.class));
+
+		final Double negativeValue = -1.3;
+		assertThat("Value must be non-negative",
+			() -> new ResourceDemandCapturedEvent(codeSection, type, negativeValue),
+			throwsException(IllegalArgumentException.class));
+
 	}
 
 	/**
 	 * Test method for {@link ResourceDemandCapturedEvent#getType()} .
 	 */
 	@Test
-	public void testGetType() {
-		fail("Not yet implemented");
+	public void getType() {
+		final CodeSection codeSection = CODE_SECTION_FACTORY.getOne();
+		ResourceDemandType type = new ResourceDemandType("Test", true);
+		final Double value = 2.0;
+		final ResourceDemandCapturedEvent event = new ResourceDemandCapturedEvent(codeSection, type, value);
+
+		assertThat(event.getType(), is(type));
+
+		type = new ResourceDemandType("new type", false);
+		assertThat(event.getType(), is(not(type)));
+
 	}
 
 	/**
 	 * Test method for {@link ResourceDemandCapturedEvent#getValue()} .
 	 */
 	@Test
-	public void testGetValue() {
-		fail("Not yet implemented");
+	public void getValue() {
+		final CodeSection codeSection = CODE_SECTION_FACTORY.getOne();
+		final ResourceDemandType type = new ResourceDemandType("Test", true);
+		Double value = 2.0;
+		final ResourceDemandCapturedEvent event = new ResourceDemandCapturedEvent(codeSection, type, value);
+
+		assertThat(event.getValue(), is(value));
+
+		value = 5.5;
+		assertThat(event.getValue(), is(not(value)));
 	}
 
 	/**
-	 * Test method for {@link ResourceDemandCapturedEvent#receive(MeasurementEventVisitor)}
-	 * .
+	 * Test method for
+	 * {@link ResourceDemandCapturedEvent#receive(MeasurementEventVisitor)} .
 	 */
 	@Test
-	public void testRecive() {
-		fail("Not yet implemented");
+	public void receive() {
+		final CodeSection codeSection = CODE_SECTION_FACTORY.getOne();
+		final ResourceDemandType type = new ResourceDemandType("Test", true);
+		final Double value = 2.0;
+		final MeasurementEventVisitor mockVisitor = mock(MeasurementEventVisitor.class);
+		final ResourceDemandCapturedEvent event = new ResourceDemandCapturedEvent(codeSection, type, value);
+		assertThat(() -> event.receive(null), throwsException(NullPointerException.class));
+		event.receive(mockVisitor);
+		then(mockVisitor).should().visit(same(event));
 	}
 
 }
