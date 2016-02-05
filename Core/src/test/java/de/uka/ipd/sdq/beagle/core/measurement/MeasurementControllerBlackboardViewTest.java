d49e32a446643e0503560e175796fdfd1d6f6d99
Flyfl <ansgar.spiegler@gmail.com>
2016-02-05 16:52:01 +0100

    Added tests to MeasurementControllerBlackboardViewTest.java

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/MeasurementControllerBlackboardViewTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/MeasurementControllerBlackboardViewTest.java
@@ -1,59 +1,267 @@
 package de.uka.ipd.sdq.beagle.core.measurement;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.theInstance;
 import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.same;
+import static org.mockito.Mockito.mock;
+
+import de.uka.ipd.sdq.beagle.core.Blackboard;
+import de.uka.ipd.sdq.beagle.core.ExternalCallParameter;
+import de.uka.ipd.sdq.beagle.core.MeasurableSeffElement;
+import de.uka.ipd.sdq.beagle.core.ResourceDemandingInternalAction;
+import de.uka.ipd.sdq.beagle.core.SeffBranch;
+import de.uka.ipd.sdq.beagle.core.SeffLoop;
+import de.uka.ipd.sdq.beagle.core.judge.EvaluableExpressionFitnessFunction;
+import de.uka.ipd.sdq.beagle.core.testutil.factories.BlackboardFactory;
+import de.uka.ipd.sdq.beagle.core.testutil.factories.EvaluableExpressionFitnessFunctionFactory;
+import de.uka.ipd.sdq.beagle.core.testutil.factories.ResourceDemandingInternalActionFactory;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Testing the {@link MeasurementControllerBlackboardView}.
+ * 
+ * @author Joshua Gleitze
+ * @author Ansgar Spiegler
+ */
 public class MeasurementControllerBlackboardViewTest {
 
+	/**
+	 * A {@link ResourceDemandingInternalAction} factory to easily obtain new instances
+	 * from.
+	 */
+	private static final ResourceDemandingInternalActionFactory RDIA_FACTORY =
+		new ResourceDemandingInternalActionFactory();
+
+	/**
+	 * A {@link SeffBranch} factory to easily obtain new instances from.
+	 */
+	private static final EvaluableExpressionFitnessFunctionFactory FITNESS_FUNCTION_FACTORY =
+		new EvaluableExpressionFitnessFunctionFactory();
+
+	/**
+	 * A {@link SeffBranch} factory to easily obtain new instances from.
+	 */
+	private static final BlackboardFactory BLACKBOARD_FACTORY = new BlackboardFactory();
+
+	/**
+	 * Blackboard mock-up that handles method calls.
+	 */
+	private Blackboard mockedBlackboard;
+
+	/**
+	 * The referenced view by this test.
+	 */
+	private MeasurementControllerBlackboardView testedView;
+
+	/**
+	 * Initializes {@link #mockedBlackboard} with Mockit and creates a new
+	 * {@link #testedView} of it.
+	 * 
+	 * @throws Exception Non specified exception because every exception in the following
+	 *             test cases result in
+	 */
 	@Before
-	public void setUp() throws Exception {
+	public void createView() {
+		this.mockedBlackboard = mock(Blackboard.class);
+		this.testedView = new MeasurementControllerBlackboardView(this.mockedBlackboard);
 	}
 
+	/**
+	 * Test method for {@link MeasurementControllerBlackboardView#getRdiasToBeMeasured()}.
+	 * Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The tested view returns the instance it obtained from the blackboard
+	 *
+	 * </ul>
+	 *
+	 */
 	@Test
 	public void testGetRdiasToBeMeasured() {
-		fail("Not yet implemented");
+		final Set<ResourceDemandingInternalAction> testInstance = new HashSet<>();
+		given(this.mockedBlackboard.getRdiasToBeMeasured()).willReturn(testInstance);
+
+		final Set<ResourceDemandingInternalAction> result = this.testedView.getRdiasToBeMeasured();
+		assertThat("The testedView should return the instance it obtained from the blackboad", result,
+			is(theInstance(testInstance)));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#getSeffBranchesToBeMeasured()}. Asserts
+	 * that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The tested view returns the instance it obtained from the blackboard
+	 *
+	 * </ul>
+	 *
+	 */
 	@Test
 	public void testGetSeffBranchesToBeMeasured() {
-		fail("Not yet implemented");
+		final Set<SeffBranch> testInstance = new HashSet<>();
+		given(this.mockedBlackboard.getSeffBranchesToBeMeasured()).willReturn(testInstance);
+
+		final Set<SeffBranch> result = this.testedView.getSeffBranchesToBeMeasured();
+		assertThat("The testedView should return the instance it obtained from the blackboad", result,
+			is(theInstance(testInstance)));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#getSeffLoopsToBeMeasured()}. Asserts
+	 * that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The tested view returns the instance it obtained from the blackboard
+	 *
+	 * </ul>
+	 *
+	 */
 	@Test
 	public void testGetSeffLoopsToBeMeasured() {
-		fail("Not yet implemented");
+		final Set<SeffLoop> testInstance = new HashSet<>();
+		given(this.mockedBlackboard.getSeffLoopsToBeMeasured()).willReturn(testInstance);
+
+		final Set<SeffLoop> result = this.testedView.getSeffLoopsToBeMeasured();
+		assertThat("The testedView should return the instance it obtained from the blackboad", result,
+			is(theInstance(testInstance)));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#getExternalCallParametersToBeMeasured()}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The tested view returns the instance it obtained from the blackboard
+	 *
+	 * </ul>
+	 *
+	 */
 	@Test
 	public void testGetExternalCallParametersToBeMeasured() {
-		fail("Not yet implemented");
+		final Set<ExternalCallParameter> testInstance = new HashSet<>();
+		given(this.mockedBlackboard.getExternalCallParametersToBeMeasured()).willReturn(testInstance);
+
+		final Set<ExternalCallParameter> result = this.testedView.getExternalCallParametersToBeMeasured();
+		assertThat("The testedView should return the instance it obtained from the blackboad", result,
+			is(theInstance(testInstance)));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#addMeasurementResultFor(MeasurableSeffElement, MesasurableSeffElementMeasurementResult)}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The call is delegated to the blackboard.
+	 *
+	 * </ul>
+	 */
 	@Test
 	public void testAddMeasurementResultForResourceDemandingInternalActionResourceDemandMeasurementResult() {
-		fail("Not yet implemented");
+		final ResourceDemandingInternalAction rdia = RDIA_FACTORY.getOne();
+		final ResourceDemandMeasurementResult results = new ResourceDemandMeasurementResult();
+
+		this.testedView.addMeasurementResultFor(rdia, results);
+		then(this.mockedBlackboard).should().addMeasurementResultFor(same(rdia), same(results));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#addMeasurementResultFor(MeasurableSeffElement, MesasurableSeffElementMeasurementResult)}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The call is delegated to the blackboard.
+	 *
+	 * </ul>
+	 */
 	@Test
 	public void testAddMeasurementResultForSeffLoopLoopRepetitionCountMeasurementResult() {
-		fail("Not yet implemented");
+		final ResourceDemandingInternalAction rdia = RDIA_FACTORY.getOne();
+		final ResourceDemandMeasurementResult results = new ResourceDemandMeasurementResult();
+
+		this.testedView.addMeasurementResultFor(rdia, results);
+		then(this.mockedBlackboard).should().addMeasurementResultFor(same(rdia), same(results));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#addMeasurementResultFor(MeasurableSeffElement, MesasurableSeffElementMeasurementResult)}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The call is delegated to the blackboard.
+	 *
+	 * </ul>
+	 */
 	@Test
 	public void testAddMeasurementResultForSeffBranchBranchDecisionMeasurementResult() {
-		fail("Not yet implemented");
+		final ResourceDemandingInternalAction rdia = RDIA_FACTORY.getOne();
+		final ResourceDemandMeasurementResult results = new ResourceDemandMeasurementResult();
+
+		this.testedView.addMeasurementResultFor(rdia, results);
+		then(this.mockedBlackboard).should().addMeasurementResultFor(same(rdia), same(results));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#addMeasurementResultFor(MeasurableSeffElement, MesasurableSeffElementMeasurementResult)}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The call is delegated to the blackboard.
+	 *
+	 * </ul>
+	 */
 	@Test
 	public void testAddMeasurementResultForExternalCallParameterParameterChangeMeasurementResult() {
-		fail("Not yet implemented");
+		final ResourceDemandingInternalAction rdia = RDIA_FACTORY.getOne();
+		final ResourceDemandMeasurementResult results = new ResourceDemandMeasurementResult();
+
+		this.testedView.addMeasurementResultFor(rdia, results);
+		then(this.mockedBlackboard).should().addMeasurementResultFor(same(rdia), same(results));
 	}
 
+	/**
+	 * Test method for {@link MeasurementControllerBlackboardView#getGetFitnessFunction()}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The tested view returns the instance it obtained from the blackboard.
+	 *
+	 * </ul>
+	 */
 	@Test
-	public void testGetFitnessFunction() {
-		fail("Not yet implemented");
+	public void getFitnessFunction() {
+		final EvaluableExpressionFitnessFunction fitnessFunction = FITNESS_FUNCTION_FACTORY.getOne();
+		given(this.mockedBlackboard.getFitnessFunction()).willReturn(fitnessFunction);
+
+		final EvaluableExpressionFitnessFunction result = this.testedView.getFitnessFunction();
+		assertThat("The testedView should return the Fitness Function instance it obtained from the blackboad", result,
+			is(theInstance(fitnessFunction)));
 	}
 
 	@Test
@@ -66,19 +274,76 @@ public class MeasurementControllerBlackboardViewTest {
 		fail("Not yet implemented");
 	}
 
+	/**
+	 * Test method for {@link MeasurementControllerBlackboardView#hashCode()} . Asserts
+	 * that:
+	 *
+	 * <ul>
+	 *
+	 * <li> The hash code is the same for equal view.
+	 *
+	 * </ul>
+	 */
 	@Test
-	public void testHashCode() {
-		fail("Not yet implemented");
+	public void hashCodeT() {
+		// Creating equal view to the existing one
+		final MeasurementControllerBlackboardView secondView =
+			new MeasurementControllerBlackboardView(this.mockedBlackboard);
+		assertThat("Two equal MeasurementControllerBlackboardViews should have the same hashCode!",
+			secondView.hashCode(), is(equalTo(this.testedView.hashCode())));
 	}
 
+	/**
+	 * Test method for
+	 * {@link MeasurementControllerBlackboardView#equals(MeasurementControllerBlackboardView)}
+	 * . Asserts that:
+	 *
+	 * <ul>
+	 *
+	 * <li> Two different views containing the same Blackboard reference are equal
+	 * 
+	 * <li> Two different views containing equal Blackboards that have not the same
+	 * reference, are not equal.
+	 *
+	 * </ul>
+	 */
 	@Test
-	public void testEquals() {
-		fail("Not yet implemented");
+	public void equalsT() {
+		// Creating equal view to the existing one
+		final MeasurementControllerBlackboardView secondView =
+			new MeasurementControllerBlackboardView(this.mockedBlackboard);
+		assertThat(
+			"Two MeasurementControllerBlackboardViews should be equal when they have the same blackboard instance!",
+			secondView, is(equalTo(this.testedView)));
+
+		// Equals for two views should only return true, if the views have exactly the
+		// same blackboard reference!
+		final Blackboard blackboardEmptyOne = BLACKBOARD_FACTORY.getEmpty();
+		final Blackboard blackboardEmptyTwo = BLACKBOARD_FACTORY.getEmpty();
+		final MeasurementControllerBlackboardView emptyViewOne =
+			new MeasurementControllerBlackboardView(blackboardEmptyOne);
+		final MeasurementControllerBlackboardView emptyViewTwo =
+			new MeasurementControllerBlackboardView(blackboardEmptyTwo);
+
+		assertThat("Two MeasurementControllerBlackboardViews should not be equal,"
+			+ "if they have not exact the same Blackboard reference!", emptyViewOne, not(equalTo(emptyViewTwo)));
 	}
 
+	/**
+	 * Test method for {@link MeasurementControllerBlackboardView#toString} . Asserts
+	 * that:
+	 *
+	 * <ul>
+	 *
+	 * <li> toString does not return the standard String as defined in Object.class.
+	 *
+	 * </ul>
+	 */
 	@Test
-	public void testToString() {
-		fail("Not yet implemented");
+	public void toStringT() {
+		final String standardRepresentation = this.testedView.getClass().getName() + "@" + this.testedView.hashCode();
+		assertThat("toString should be overwritten by a meaningful representation of this object!",
+			standardRepresentation, not(equalTo(this.testedView.toString())));
 	}
 
 }
