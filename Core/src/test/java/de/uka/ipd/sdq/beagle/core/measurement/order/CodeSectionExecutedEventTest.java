2461eb24e4df672ec5c19c0ba8bee99c90a3b24a
Annika Berger <annika.c.berger@gmail.com>
2016-02-05 12:27:51 +0100

    implemented parts of CodeSectionExecutedEventTest

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/CodeSectionExecutedEventTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/CodeSectionExecutedEventTest.java
@@ -1,8 +1,10 @@
 package de.uka.ipd.sdq.beagle.core.measurement.order;
 
-import static org.junit.Assert.fail;
+import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 import de.uka.ipd.sdq.beagle.core.CodeSection;
+import de.uka.ipd.sdq.beagle.core.testutil.factories.CodeSectionFactory;
 
 import org.junit.Test;
 
@@ -15,20 +17,32 @@ import org.junit.Test;
 public class CodeSectionExecutedEventTest {
 
 	/**
+	 * A {@link CodeSectionFactory} providing methods to get code sections to use for
+	 * tests.
+	 */
+	private static final CodeSectionFactory CODE_SECTION_FACTORY = new CodeSectionFactory();
+
+	/**
 	 * Test method for
 	 * {@link CodeSectionExecutedEvent#CodeSectionExecutedEvent(CodeSection)}.
 	 */
 	@Test
 	public void constructor() {
-		fail("Not yet implemented");
+		final CodeSection[] codeSections = CODE_SECTION_FACTORY.getAll();
+		for (CodeSection codeSection : codeSections) {
+			new CodeSectionExecutedEvent(codeSection);
+		}
+
+		assertThat("Code section must not be null", () -> new CodeSectionExecutedEvent(null),
+			throwsException(NullPointerException.class));
 	}
 
 	/**
 	 * Test method for {@link CodeSectionExecutedEvent#receive(MeasurementEventVisitor)}.
 	 */
 	@Test
-	public void recive() {
-		fail("Not yet implemented");
+	public void receive() {
+		
 	}
 
 }
