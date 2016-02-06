f635ed80472110c92b9cd8115e64bc7cd2ada9c2
Michael Vogt <mvogt24@googlemail.com>
2016-02-06 12:11:14 +0100

    changed finalExpressions changed setFinalExpressionFor method changed getFinalExpressionFor method

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/Blackboard.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/Blackboard.java
@@ -115,7 +115,7 @@ public class Blackboard implements Serializable {
 	/**
 	 * {@code finalExpression} is the final expression.
 	 */
-	private Map<MeasurableSeffElement, Set<EvaluableExpression>> finalExpressions;
+	private Map<MeasurableSeffElement, EvaluableExpression> finalExpressions;
 
 	/**
 	 * {@code fitnissFunction} is the function to get a better evaluable expression
@@ -543,8 +543,7 @@ public class Blackboard implements Serializable {
 	 * @return The expression momentarily marked to be the final for {@code element}.
 	 *         {@code null} if no expression has been marked yet.
 	 */
-	public Map<MeasurableSeffElement, Set<EvaluableExpression>> getFinalExpressionFor(
-		final MeasurableSeffElement element) {
+	public Map<MeasurableSeffElement, EvaluableExpression> getFinalExpressionFor(final MeasurableSeffElement element) {
 		Validate.notNull(element);
 		Validate.notNull(this.finalExpressions);
 		return this.finalExpressions;
@@ -563,7 +562,7 @@ public class Blackboard implements Serializable {
 	public void setFinalExpressionFor(final MeasurableSeffElement element, final Set<EvaluableExpression> expression) {
 		Validate.notNull(element);
 		Validate.notNull(expression);
-		this.finalExpressions.put(element, expression);
+		this.finalExpressions.get(element);
 	}
 
 	/**
