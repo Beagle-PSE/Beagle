a5fa315038554c20e21b9e70fb2ac625ba1c3146
Roman Langrehr <roman.langrehr@yahoo.de>
2016-02-06 20:07:17 +0100

    Implemented MeasurementEvent parsing for SeffLoops.

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/measurement/order/CodeSectionLeftEvent.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/measurement/order/CodeSectionLeftEvent.java
@@ -1,12 +1,31 @@
 package de.uka.ipd.sdq.beagle.core.measurement.order;
 
+import de.uka.ipd.sdq.beagle.core.CodeSection;
 
+/**
+ * An event for the fact that a code section was being <em>left</em> to be executed. This
+ * does imply that the <em>full</em> section was executed and a
+ * {@link CodeSectionEnteredEvent} was created <em>before</em>. However, a code section
+ * should usually be defined in a way that does not allow the control flow to enter but
+ * not completely execute it.
+ *
+ * @author Joshua Gleitze
+ * @author Roman Langrehr
+ */
 public class CodeSectionLeftEvent extends AbstractMeasurementEvent {
 
-	@Override
-	public void receive(MeasurementEventVisitor visitor) {
-		// TODO Auto-generated method stub
+	/**
+	 * Creates an event for the fact that {@code codeSection} was stopped to be executed.
+	 *
+	 * @param codeSection The section that was executed. Must not be {@code null}.
+	 */
+	public CodeSectionLeftEvent(final CodeSection codeSection) {
+		super(codeSection);
+	}
 
+	@Override
+	public void receive(final MeasurementEventVisitor visitor) {
+		visitor.visit(this);
 	}
 
 }
