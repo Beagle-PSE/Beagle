ea754efd8f37f0c28e8105b88c435a7fdd357b08
Annika Berger <annika.c.berger@gmail.com>
2016-02-07 17:49:00 +0100

    fixed MeasurementOrderTest by removing a launchConfiguration of the set (which was in before)

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/MeasurementOrderTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/MeasurementOrderTest.java
@@ -151,7 +151,7 @@ public class MeasurementOrderTest {
 
 		assertThat(measurementOrder.getLaunchConfigurations(), is(equalTo(launchConfigurations)));
 
-		launchConfigurations.remove(LAUNCH_CONFIGURATION_FACTORY.getOne());
+		launchConfigurations.remove(launchConfigurations.toArray()[0]);
 		assertThat(
 			"Changing the Set of Launch Configurations after inizialisation of the Measurement order must not change the result.",
 			measurementOrder.getLaunchConfigurations(), is(not(equalTo(launchConfigurations))));
