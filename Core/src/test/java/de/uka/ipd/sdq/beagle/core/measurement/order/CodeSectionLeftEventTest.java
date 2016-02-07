d0b42d9db84189f3bd2145dab4173c2bbb1dceca
Annika Berger <annika.c.berger@gmail.com>
2016-02-07 11:14:23 +0100

    converted Test for CodeSectionEcexutedEvent into ones for CodeSectionEnteredEvent and CodeSectionLeftEvent

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/CodeSectionLeftEventTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/measurement/order/CodeSectionLeftEventTest.java
@@ -2,8 +2,8 @@ package de.uka.ipd.sdq.beagle.core.measurement.order;
 
 import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.BDDMockito.same;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Matchers.same;
 import static org.mockito.Mockito.mock;
 
 import de.uka.ipd.sdq.beagle.core.CodeSection;
@@ -12,13 +12,13 @@ import de.uka.ipd.sdq.beagle.core.testutil.factories.CodeSectionFactory;
 import org.junit.Test;
 
 /**
- * Tests {@link CodeSectionEnteredEvent} and contains all test cases needed to check
- * every method.
+ * Tests {@link CodeSectionLeftEvent} and contains the test cases needed to check all
+ * methods.
  * 
  * @author Annika Berger
  */
-public class CodeSectionExecutedEventTest {
-
+public class CodeSectionLeftEventTest {
+	
 	/**
 	 * A {@link CodeSectionFactory} providing methods to get code sections to use for
 	 * tests.
@@ -26,28 +26,27 @@ public class CodeSectionExecutedEventTest {
 	private static final CodeSectionFactory CODE_SECTION_FACTORY = new CodeSectionFactory();
 
 	/**
-	 * Test method for
-	 * {@link CodeSectionEnteredEvent#CodeSectionExecutedEvent(CodeSection)}.
+	 * Test method for {@link CodeSectionLeftEvent#CodeSectionLeftEvent(CodeSection)}.
 	 */
 	@Test
 	public void constructor() {
 		final CodeSection[] codeSections = CODE_SECTION_FACTORY.getAll();
 		for (CodeSection codeSection : codeSections) {
-			new CodeSectionEnteredEvent(codeSection);
+			new CodeSectionLeftEvent(codeSection);
 		}
 
-		assertThat("Code section must not be null", () -> new CodeSectionEnteredEvent(null),
+		assertThat("Code section must not be null", () -> new CodeSectionLeftEvent(null),
 			throwsException(NullPointerException.class));
 	}
 
 	/**
-	 * Test method for {@link CodeSectionEnteredEvent#receive(MeasurementEventVisitor)}.
+	 * Test method for {@link CodeSectionLeftEvent#receive(MeasurementEventVisitor)}.
 	 */
 	@Test
 	public void receive() {
 		final CodeSection codeSection = CODE_SECTION_FACTORY.getOne();
 		final MeasurementEventVisitor mockVisitor = mock(MeasurementEventVisitor.class);
-		final CodeSectionEnteredEvent event = new CodeSectionEnteredEvent(codeSection);
+		final CodeSectionLeftEvent event = new CodeSectionLeftEvent(codeSection);
 		assertThat(() -> event.receive(null), throwsException(NullPointerException.class));
 		event.receive(mockVisitor);
 		then(mockVisitor).should().visit(same(event));
