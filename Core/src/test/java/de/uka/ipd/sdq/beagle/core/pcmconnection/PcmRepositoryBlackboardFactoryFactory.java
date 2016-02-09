08686f96a7d00557a7305717836da361d96d43e0
Flyfl <ansgar.spiegler@gmail.com>
2016-02-09 18:23:30 +0100

    Added an EvaluableExpressionFitnessFunctionFactory to BlackboardFactoryFactory for test cases

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/pcmconnection/PcmRepositoryBlackboardFactoryFactory.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/pcmconnection/PcmRepositoryBlackboardFactoryFactory.java
@@ -1,20 +1,36 @@
 package de.uka.ipd.sdq.beagle.core.pcmconnection;
 
+import de.uka.ipd.sdq.beagle.core.judge.EvaluableExpressionFitnessFunction;
+import de.uka.ipd.sdq.beagle.core.testutil.factories.EvaluableExpressionFitnessFunctionFactory;
+
+import java.io.FileNotFoundException;
+
 /**
  * A factory which creates instances of {@link PcmRepositoryBlackboardFactory}.
  * 
  * @author Christoph Michelbach
+ * @author Ansgar Spiegler
  */
 public class PcmRepositoryBlackboardFactoryFactory {
+
+	/**
+	 * An {@link EvaluableExpressionFitnessFunction} factory to easily obtain new
+	 * instances from.
+	 */
+	private static final EvaluableExpressionFitnessFunctionFactory FITNESS_FUNCTION_FACTORY =
+		new EvaluableExpressionFitnessFunctionFactory();
 
 	/**
 	 * Returns a valid {@link PcmRepositoryBlackboardFactory} object.
 	 *
 	 * @return A valid {@link PcmRepositoryBlackboardFactory} object.
+	 * @throws FileNotFoundException When Constructor of
+	 *             {@link PcmRepositoryBlackboardFactory} does not get a valid file
 	 */
-	public PcmRepositoryBlackboardFactory getValidInstance() {
+	public PcmRepositoryBlackboardFactory getValidInstance() throws FileNotFoundException {
 		return new PcmRepositoryBlackboardFactory(
-			"src/test/resources/de/uka/ipd/sdq/beagle/core/pcmconnection/Family.repository");
+			"src/test/resources/de/uka/ipd/sdq/beagle/core/pcmconnection/Family.repository",
+			PcmRepositoryBlackboardFactoryFactory.FITNESS_FUNCTION_FACTORY.getOne());
 	}
 
 }
