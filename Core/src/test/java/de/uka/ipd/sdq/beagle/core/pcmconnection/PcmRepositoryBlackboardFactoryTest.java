08666ae38e0efa011f486cb4a5b7fae3d68237f9
m1cm1c <michelbach94@gmail.com>
2016-02-09 21:13:42 +0100

    Introduced pseudo fitness function.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/pcmconnection/PcmRepositoryBlackboardFactoryTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/pcmconnection/PcmRepositoryBlackboardFactoryTest.java
@@ -9,15 +9,21 @@ import static org.hamcrest.Matchers.nullValue;
 
 import de.uka.ipd.sdq.beagle.core.Blackboard;
 import de.uka.ipd.sdq.beagle.core.ExternalCallParameter;
+import de.uka.ipd.sdq.beagle.core.MeasurableSeffElement;
 import de.uka.ipd.sdq.beagle.core.ResourceDemandingInternalAction;
 import de.uka.ipd.sdq.beagle.core.SeffBranch;
 import de.uka.ipd.sdq.beagle.core.SeffLoop;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.EvaluableExpression;
+import de.uka.ipd.sdq.beagle.core.judge.EvaluableExpressionFitnessFunction;
+import de.uka.ipd.sdq.beagle.core.judge.EvaluableExpressionFitnessFunctionBlackboardView;
 import de.uka.ipd.sdq.beagle.core.testutil.factories.BlackboardFactory;
 
+import org.eclipse.net4j.util.collection.Pair;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.HashSet;
 
 /**
@@ -34,6 +40,62 @@ public class PcmRepositoryBlackboardFactoryTest {
 		new PcmRepositoryBlackboardFactoryFactory();
 
 	/**
+	 * Pseudo fitness function.
+	 */
+	private EvaluableExpressionFitnessFunction fitnessFunction = new EvaluableExpressionFitnessFunction() {
+
+		/**
+		 * Store between (pairs of measurable seff elements and evaluable expressions) and
+		 * doubles.
+		 */
+		private HashMap<Pair<MeasurableSeffElement, EvaluableExpression>, Double> store = new HashMap<>();
+
+		@Override
+		public double gradeFor(final ExternalCallParameter parameter, final EvaluableExpression expression,
+			final EvaluableExpressionFitnessFunctionBlackboardView blackboard) {
+			return this.determineValue(parameter, expression);
+		}
+
+		@Override
+		public double gradeFor(final SeffLoop loop, final EvaluableExpression expression,
+			final EvaluableExpressionFitnessFunctionBlackboardView blackboard) {
+			return this.determineValue(loop, expression);
+		}
+
+		@Override
+		public double gradeFor(final SeffBranch branch, final EvaluableExpression expression,
+			final EvaluableExpressionFitnessFunctionBlackboardView blackboard) {
+			return this.determineValue(branch, expression);
+		}
+
+		@Override
+		public double gradeFor(final ResourceDemandingInternalAction rdia, final EvaluableExpression expression,
+			final EvaluableExpressionFitnessFunctionBlackboardView blackboard) {
+			return this.determineValue(rdia, expression);
+		}
+
+		/**
+		 * Determines the fitness of a combination of a {@code MeasurableSeffElement} and
+		 * an {@code EvaluableExpression}.
+		 *
+		 * @param element A {@code MeasurableSeffElement}.
+		 * @param expression An {@code EvaluableExpression}.
+		 * @return The fitness value.
+		 */
+		private double determineValue(final MeasurableSeffElement element, final EvaluableExpression expression) {
+			final Pair<MeasurableSeffElement, EvaluableExpression> pair = new Pair<>(parameter, expression);
+			if (this.store.containsKey(pair)) {
+				return this.store.get(pair);
+			} else {
+				final double fitness = Math.pow(Math.random() * 10E5, 3);
+				this.store.put(pair, fitness);
+
+				return fitness;
+			}
+		}
+	};
+
+	/**
 	 * Test method for
 	 * {@link PcmRepositoryBlackboardFactory#PcmRepositoryBlackboardFactory(java.util.Set)
 	 * and PcmRepositoryBlackboardFactory#PcmRepositoryBlackboardFactory(String)}. Asserts
@@ -42,22 +104,29 @@ public class PcmRepositoryBlackboardFactoryTest {
 	 */
 	@Test
 	public void pcmRepositoryBlackboardFactory() {
-		assertThat(() -> new PcmRepositoryBlackboardFactory((String) null),
+		assertThat(() -> new PcmRepositoryBlackboardFactory((String) null, this.fitnessFunction),
 			throwsException(NullPointerException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory((File) null), throwsException(NullPointerException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory(""), throwsException(IllegalArgumentException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory("."), throwsException(IllegalArgumentException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory(".."), throwsException(IllegalArgumentException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory("/"), throwsException(IllegalArgumentException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory("/tmp"), throwsException(IllegalArgumentException.class));
-		assertThat(() -> new PcmRepositoryBlackboardFactory("\0"), throwsException(IllegalArgumentException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory((File) null, this.fitnessFunction),
+			throwsException(NullPointerException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory("", this.fitnessFunction),
+			throwsException(IllegalArgumentException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory(".", this.fitnessFunction),
+			throwsException(IllegalArgumentException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory("..", this.fitnessFunction),
+			throwsException(IllegalArgumentException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory("/", this.fitnessFunction),
+			throwsException(IllegalArgumentException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory("/tmp", this.fitnessFunction),
+			throwsException(IllegalArgumentException.class));
+		assertThat(() -> new PcmRepositoryBlackboardFactory("\0", this.fitnessFunction),
+			throwsException(IllegalArgumentException.class));
 
 		final File[] impossibleRepositoryFiles = {
 			new File(""), new File("."), new File(".."), new File("/"), new File("/tmp"), new File("\0")
 		};
 
 		for (File impossibleRepositoryFile : impossibleRepositoryFiles) {
-			assertThat(() -> new PcmRepositoryBlackboardFactory(impossibleRepositoryFile),
+			assertThat(() -> new PcmRepositoryBlackboardFactory(impossibleRepositoryFile, this.fitnessFunction),
 				throwsException(IllegalArgumentException.class));
 		}
 	}
@@ -126,8 +195,9 @@ public class PcmRepositoryBlackboardFactoryTest {
 		assertThat(pcmRepositoryBlackboardFactory.getBlackboardForIds("_6f1a4LnmEeWVlphM5rov7g"),
 			is(not(pcmRepositoryBlackboardFactory.getBlackboardForAllElements())));
 
-		final Blackboard emptyBlackboard = new Blackboard(new HashSet<ResourceDemandingInternalAction>(),
-			new HashSet<SeffBranch>(), new HashSet<SeffLoop>(), new HashSet<ExternalCallParameter>());
+		final Blackboard emptyBlackboard =
+			new Blackboard(new HashSet<ResourceDemandingInternalAction>(), new HashSet<SeffBranch>(),
+				new HashSet<SeffLoop>(), new HashSet<ExternalCallParameter>(), this.fitnessFunction);
 
 		assertThat(pcmRepositoryBlackboardFactory.getBlackboardForIds("_SomeIdWhichDosntExistA"), is(emptyBlackboard));
 
