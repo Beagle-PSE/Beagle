de564721061c050996dbf69782c644abd9b27b2b
Michael Vogt <mvogt24@googlemail.com>
2016-02-12 17:08:22 +0100

    adapt beagleConfigurationTest to package ...core/facade

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/facade/BeagleConfigurationTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/facade/BeagleConfigurationTest.java
@@ -1,4 +1,4 @@
-package de.uka.ipd.sdq.beagle.core;
+package de.uka.ipd.sdq.beagle.core.facade;
 
 import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
 import static org.hamcrest.Matchers.is;
