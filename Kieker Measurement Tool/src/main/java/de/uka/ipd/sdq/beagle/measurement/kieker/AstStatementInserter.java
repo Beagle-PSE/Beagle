c34cb5b5171ce94681fad3663c3a06c1a4c4d820
Joshua Gleitze <git@joshuagleitze.de>
2016-02-12 22:52:28 +0100

    Heavy improvements for AstStatementInserter

--- Kieker Measurement Tool/src/main/java/de/uka/ipd/sdq/beagle/measurement/kieker/AstStatementInserter.java
+++ Kieker Measurement Tool/src/main/java/de/uka/ipd/sdq/beagle/measurement/kieker/AstStatementInserter.java
@@ -7,58 +7,93 @@ import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.StructuralPropertyDescriptor;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BiConsumer;
 
 /**
  * Inserts statements into an Eclipse AST. To do so, it finds a parent AST node into which
  * statements can be added. If found, it adds a provided statement into that node, either
- * directly after or directly before the statement that contains the provided marker
- * statement.
+ * directly after or directly before the node that contains node this inserter was created
+ * for.
  *
  * @author Joshua Gleitze
  */
 class AstStatementInserter {
 
 	/**
-	 * A visitor performing the actual insertion.
+	 * The node marking where to insert.
 	 */
-	private final InsertionPositionFinder inserter = new InsertionPositionFinder();
+	private final ASTNode marker;
 
 	/**
-	 * Inserts the provided {@code statement} before the provided {@code marker}. Tries to
-	 * put the {@code statement} as close to the {@code marker} as possible.
+	 * An optional function capable of executing the insertion. Takes the statement to
+	 * insert and an offset defining how far off to insert (0 for directly before, 1 for
+	 * directly after). {@code null} indicates that we did not yet search the function. If
+	 * the optional is not present, there is no such function.
+	 */
+	private Optional<BiConsumer<Statement, Integer>> insertionStrategy;
+
+	/**
+	 * Creates an inserter that will insert statements around the provided {@code marker}.
 	 *
-	 * @param marker The statement before which {@code statement} shall be inserted.
-	 * @param statement The statement to insert before {@code marker}.
+	 * @param marker The node around which insertion is requested.
+	 */
+	AstStatementInserter(final ASTNode marker) {
+		this.marker = marker;
+	}
+
+	/**
+	 * Inserts the provided {@code statement} before the node this inserter was created
+	 * for. Tries to put the {@code statement} as close to the node as possible.
+	 *
+	 * @param statement The statement to insert before the insertion node.
 	 * @return {@code true} if {@code statement} was inserted. {@code false} if there is
 	 *         no point in the AST where {@code statement} could validly be inserted.
 	 */
-	boolean insertBefore(final ASTNode marker, final Statement statement) {
-		Validate.notNull(marker);
+	boolean insertBefore(final Statement statement) {
 		Validate.notNull(statement);
 		// insert at the marker’s position.
-		return this.inserter.insert(marker, statement, 0);
+		return this.insert(statement, 1);
 	}
 
 	/**
-	 * Inserts the provided {@code statement} after the provided {@code marker}. Tries to
-	 * put the {@code statement} as close to the {@code marker} as possible.
+	 * Inserts the provided {@code statement} after the provided the node this inserter
+	 * was created for. Tries to put the {@code statement} as close to the {@code marker}
+	 * as possible.
 	 *
-	 * @param marker The statement after which {@code statement} shall be inserted.
-	 * @param statement The statement to insert after {@code marker}.
+	 * @param statement The statement to insert after the insertion node.
 	 * @return {@code true} if {@code statement} was inserted. {@code false} if there is
 	 *         no point in the AST where {@code statement} could validly be inserted.
 	 */
-	boolean insertAfter(final ASTNode marker, final Statement statement) {
-		Validate.notNull(marker);
+	boolean insertAfter(final Statement statement) {
 		Validate.notNull(statement);
 		// insert 1 after the marker’s position.
-		return this.inserter.insert(marker, statement, 1);
+		return this.insert(statement, 1);
 	}
 
 	/**
-	 * Looks for the right position to insert the statement. Does so by traversing upwards
-	 * from the marker statement until an AST node to insert at is success. Throws an
-	 * exception if there is no such AST node.
+	 * Executes the insertion by first finding the insertion strategy and then executing
+	 * it.
+	 *
+	 * @param statement The statement to insert.
+	 * @param offset how far off to insert (0 for directly before, 1 for directly after).
+	 * @return {@code true} if {@code statement} was inserted. {@code false} if there is
+	 *         no point in the AST where {@code statement} could validly be inserted.
+	 */
+	private boolean insert(final Statement statement, final int offset) {
+		if (this.insertionStrategy == null) {
+			new InsertionPositionFinder().find();
+		}
+		if (this.insertionStrategy.isPresent()) {
+			this.insertionStrategy.get().accept(statement, offset);
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Looks for the right position to insert statements. Does so by traversing upwards
+	 * from the marker statement until an AST node to insert at is found.
 	 *
 	 * @author Joshua Gleitze
 	 */
@@ -70,37 +105,18 @@ class AstStatementInserter {
 		private ASTNode childToSearch;
 
 		/**
-		 * The statement we’ll insert.
-		 */
-		private Statement statementToInsert;
-
-		/**
-		 * The offset for insertion.
-		 */
-		private int offset;
-
-		/**
 		 * Indicates whether we managed to insert.
 		 */
 		private boolean success;
 
 		/**
-		 * Inserts {@code statement} next to {@code marker}, as far off as defined by
-		 * {@code offset}.
-		 *
-		 * @param marker The ast node indicationg where to insert.
-		 * @param statement The statement to insert.
-		 * @param offsetToMarker The offset, indicating how far off the statement as to be
-		 *            inserted. 0 is directly before, 1 is directly after.
-		 * @return Whether insertion was successful.
+		 * Searches for the insertion point and populates
+		 * {@link AstStatementInserter#insertionStrategy}.
 		 */
-		private boolean insert(final ASTNode marker, final Statement statement, final int offsetToMarker) {
+		private void find() {
 			this.success = false;
-			this.childToSearch = marker;
-			this.statementToInsert = statement;
-			this.offset = offsetToMarker;
-			marker.getParent().accept(this);
-			return this.success;
+			this.childToSearch = AstStatementInserter.this.marker;
+			this.childToSearch.getParent().accept(this);
 		}
 
 		@Override
@@ -110,6 +126,10 @@ class AstStatementInserter {
 				final ASTNode nextTry = node.getParent();
 				if (nextTry != null) {
 					nextTry.accept(this);
+				} else {
+					// we did not find a point to insert and have nowhere else to look. We
+					// failed.
+					AstStatementInserter.this.insertionStrategy = Optional.empty();
 				}
 			}
 		}
@@ -123,8 +143,10 @@ class AstStatementInserter {
 
 			@SuppressWarnings("unchecked")
 			final List<ASTNode> listToInsertIn = (List<ASTNode>) node.getStructuralProperty(propertyToInsertIn);
+			final int indexToInsertAt = listToInsertIn.indexOf(this.childToSearch);
 
-			listToInsertIn.add(listToInsertIn.indexOf(this.childToSearch) + this.offset, this.statementToInsert);
+			AstStatementInserter.this.insertionStrategy = Optional
+				.of((statementToInsert, offset) -> listToInsertIn.add(indexToInsertAt + offset, statementToInsert));
 
 			this.success = true;
 			return false;
