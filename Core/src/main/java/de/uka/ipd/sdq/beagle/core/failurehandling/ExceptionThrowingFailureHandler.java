fe95bf0e283bfcc66b28a5a44f39de72663d3ed9
Joshua Gleitze <git@joshuagleitze.de>
2016-02-13 20:55:24 +0100

    Added and used FailureException

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/failurehandling/ExceptionThrowingFailureHandler.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/failurehandling/ExceptionThrowingFailureHandler.java
@@ -31,7 +31,31 @@ public class ExceptionThrowingFailureHandler extends FailureHandler {
 		if (report.getDetails() != null) {
 			failureDescription += "\n\nDetails:\n" + report.getDetails();
 		}
-		throw new RuntimeException(failureDescription, report.getFailureCause());
+		throw new FailureException(failureDescription, report.getFailureCause());
+	}
+
+	/**
+	 * Thrown by this handler for reported failures.
+	 *
+	 * @author Joshua Gleitze
+	 */
+	public final class FailureException extends RuntimeException {
+
+		/**
+		 * Serialisation version UID, see {@link java.io.Serializable}.
+		 */
+		private static final long serialVersionUID = 1653063728785911804L;
+
+		/**
+		 * Creates the exception using the provided {@code message} and {@code cause}.
+		 *
+		 * @param message A message describing the failures.
+		 * @param cause The throwable causing or indicating the failure.
+		 */
+		private FailureException(final String message, final Throwable cause) {
+			super(message, cause);
+		}
+
 	}
 
 }
