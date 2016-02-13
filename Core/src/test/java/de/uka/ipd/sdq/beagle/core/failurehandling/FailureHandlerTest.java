a1819a2f01f9fa7f3b02280caab64c53dca955d5
Michael Vogt <mvogt24@googlemail.com>
2016-02-13 17:48:55 +0100

    create empty test class.

---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/failurehandling/FailureHandlerTest.java
@@ -0,0 +1,17 @@
+package de.uka.ipd.sdq.beagle.core.failurehandling;
+
+import org.junit.Test;
+
+/**
+ * TODO Document this type.
+ * 
+ * @author Micha
+ */
+public class FailureHandlerTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
