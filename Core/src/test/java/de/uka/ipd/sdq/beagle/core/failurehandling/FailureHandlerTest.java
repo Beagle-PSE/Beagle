5ef953f7c23ebe97804bf4371d56d203d4ab8a5f
Michael Vogt <mvogt24@googlemail.com>
2016-02-13 22:40:28 +0100

    added Tests.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/failurehandling/FailureHandlerTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/failurehandling/FailureHandlerTest.java
@@ -1,16 +1,33 @@
 package de.uka.ipd.sdq.beagle.core.failurehandling;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import org.junit.Test;
 
 /**
  * TODO Document this type.
  * 
- * @author Micha
+ * @author Michael Vogt
  */
 public class FailureHandlerTest {
 
 	@Test
-	public void test() {
+	public void getHandler() {
+		final String clientName = "clientAllice";
+		assertThat(FailureHandler.getHandler(clientName), is("clientAllice"));
+
+		final Class<?> clientType;
+		assertThat(FailureHandler.getHandler(clientType), is(clientType));
+	}
+
+	@Test
+	public void FailureHandlerProvider() {
+
+	}
+
+	@Test
+	public void setProvider() {
 
 	}
 
