bd85f5cf833f5b30c0c5e8a57012d9705c6430fa
Annika Berger <annika.c.berger@gmail.com>
2016-02-13 12:54:26 +0100

    removed COVERAGE:OFF lables from all classes

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/measurement/order/MeasurementOrder.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/measurement/order/MeasurementOrder.java
@@ -1,10 +1,4 @@
 package de.uka.ipd.sdq.beagle.core.measurement.order;
-/**
- * ATTENTION: Test coverage check turned off. Remove this comments block when implementing
- * this class!
- * 
- * <p>COVERAGE:OFF
- */
 
 /*
  * ATTENTION: Checkstyle turned off! remove this comment block when implementing this
