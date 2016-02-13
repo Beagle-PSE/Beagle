dd1854296ef1ee97e501117bcf578b309a5c0737
Annika Berger <annika.c.berger@gmail.com>
2016-02-13 21:36:54 +0100

    created Test stubs for AbstractionAndPrecisionFitnessFunction

---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/judge/AbstractionAndPrecisionFitnessFunctionTest.java
@@ -0,0 +1,55 @@
+package de.uka.ipd.sdq.beagle.core.judge;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link AbstractionAndPrecisionFitnessFunction} and contains all test cases needed
+ * to check every method.
+ * 
+ * @author Annika Berger
+ */
+public class AbstractionAndPrecisionFitnessFunctionTest {
+
+	/**
+	 * Test method for
+	 * {@link AbstractionAndPrecisionFitnessFunction#gradeFor(ResourceDemandingInternalAction, EvaluableExpression, EvaluableExpressionFitnessFunctionBlackboardView)}
+	 * .
+	 */
+	@Test
+	public void gradeForResourceDemandingInternalActionEvaluableExpressionEvaluableExpressionFitnessFunctionBlackboardView() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link AbstractionAndPrecisionFitnessFunction#gradeFor(SeffBranch, EvaluableExpression, EvaluableExpressionFitnessFunctionBlackboardView)}
+	 * .
+	 */
+	@Test
+	public void gradeForSeffBranchEvaluableExpressionEvaluableExpressionFitnessFunctionBlackboardView() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link AbstractionAndPrecisionFitnessFunction#gradeFor(SeffLoop, EvaluableExpression, EvaluableExpressionFitnessFunctionBlackboardView)}
+	 * .
+	 */
+	@Test
+	public void gradeForSeffLoopEvaluableExpressionEvaluableExpressionFitnessFunctionBlackboardView() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for
+	 * {@link AbstractionAndPrecisionFitnessFunction#gradeFor(ExternalCallParameter, EvaluableExpression, EvaluableExpressionFitnessFunctionBlackboardView)}
+	 * .
+	 */
+	@Test
+	public void gradeForExternalCallParameterEvaluableExpressionEvaluableExpressionFitnessFunctionBlackboardView() {
+		fail("Not yet implemented");
+	}
+
+}
