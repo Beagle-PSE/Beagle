f4675449c3bcb5bc279d16ab97ab580e2b95a471
Roman Langrehr <roman.langrehr@yahoo.de>
2016-02-14 14:14:39 +0100

    Adapted tests to the SourceCodeFileProvider.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/pcmconnection/PcmRepositoryBlackboardFactoryFactory.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/pcmconnection/PcmRepositoryBlackboardFactoryFactory.java
@@ -1,5 +1,12 @@
 package de.uka.ipd.sdq.beagle.core.pcmconnection;
 
+import de.uka.ipd.sdq.beagle.core.facade.SourceCodeFileProvider;
+
+import java.io.File;
+import java.io.Serializable;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 /**
  * A factory which creates instances of {@link PcmRepositoryBlackboardFactoryAdder}.
  *
@@ -10,13 +17,21 @@ package de.uka.ipd.sdq.beagle.core.pcmconnection;
 public class PcmRepositoryBlackboardFactoryFactory {
 
 	/**
+	 * A {@link SourceCodeFileProvider} that resolves paths correctly for the AppSensor
+	 * test project.
+	 */
+	public static final SourceCodeFileProvider APP_SENSOR_TEST_SOURCE_CODE_FILE_PROVIDER =
+		new AppSensorTestSourceCodeFileProvider();
+
+	/**
 	 * Returns a valid {@link PcmRepositoryBlackboardFactoryAdder} object.
 	 *
 	 * @return A valid {@link PcmRepositoryBlackboardFactoryAdder} object.
 	 */
 	public PcmRepositoryBlackboardFactoryAdder getValidInstance() {
 		return new PcmRepositoryBlackboardFactoryAdder(
-			"src/test/resources/de/uka/ipd/sdq/beagle/core/pcmconnection/Family.repository");
+			"src/test/resources/de/uka/ipd/sdq/beagle/core/pcmconnection/Family.repository",
+			APP_SENSOR_TEST_SOURCE_CODE_FILE_PROVIDER);
 	}
 
 	/**
@@ -28,7 +43,38 @@ public class PcmRepositoryBlackboardFactoryFactory {
 	 */
 	public PcmRepositoryBlackboardFactoryAdder getAppSensorProjectInstance() {
 		return new PcmRepositoryBlackboardFactoryAdder(
-			"src/test/resources/de/uka/ipd/sdq/beagle/core/pcmconnection/AppSensor.repository");
+			"src/test/resources/de/uka/ipd/sdq/beagle/core/pcmconnection/AppSensor.repository",
+			APP_SENSOR_TEST_SOURCE_CODE_FILE_PROVIDER);
 	}
 
+	/**
+	 * A {@link SourceCodeFileProvider} that resolves paths correctly for the AppSensor
+	 * test project.
+	 *
+	 * @author Roman Langrehr
+	 */
+	private static class AppSensorTestSourceCodeFileProvider implements SourceCodeFileProvider {
+
+		/**
+		 * See {@link Serializable}.
+		 */
+		private static final long serialVersionUID = -1066585975466213430L;
+
+		@Override
+		public File getSourceFile(final String fullyQualifiedJavaPath) {
+			final URL fileUrl = PcmRepositoryBlackboardFactoryFactory.class
+				.getResource("/de/uka/ipd/sdq/beagle/core/pcmconnection/Source files/"
+					+ fullyQualifiedJavaPath.replace(".", "/") + ".java");
+			if (fileUrl != null) {
+				try {
+					return new File(fileUrl.toURI());
+				} catch (final URISyntaxException uriSyntaxException) {
+					return null;
+				}
+			} else {
+				return null;
+			}
+		}
+
+	}
 }
