a862e894a6dd3298aa37c74ad559196015de796b
m1cm1c <michelbach94@gmail.com>
2016-02-23 15:37:33 +0100

    Changed `Timeout` from interface to abstract class and added functionality.

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/timeout/Timeout.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/timeout/Timeout.java
@@ -1,17 +1,29 @@
 package de.uka.ipd.sdq.beagle.core.timeout;
 
 /**
- * Decides when a timeout is reached. This interface has implementing classes which do so
- * depending on different criteria.
+ * Decides when a timeout is reached. This abstract class has implementing classes which
+ * do so depending on different criteria.
  *
  * @author Christoph Michelbach
  */
-public interface Timeout {
+public abstract class Timeout {
+
+	/**
+	 * The millisecond timestamp when this Timeout object has been initialised.
+	 */
+	protected long startingTime;
 
 	/**
 	 * Determines whether the timeout is reached.
 	 *
 	 * @return {@code true} if the timeout is reached; {@code false} otherwise.
 	 */
-	boolean isReached();
+	abstract boolean isReached();
+
+	/**
+	 * Initialises the timeout object. Sets the starting time to the current time.
+	 */
+	void init() {
+		this.startingTime = System.currentTimeMillis();
+	}
 }
