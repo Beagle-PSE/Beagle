d3b29b1fb355615f77b5f65a5dc79b0c6347a43f
Michael Vogt <mvogt24@googlemail.com>
2016-02-27 21:14:57 +0100

    added RegressionLineTest class.

---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/RegressionLineTest.java
@@ -0,0 +1,18 @@
+package de.uka.ipd.sdq.beagle.core.timeout;
+
+import org.junit.Test;
+
+/**
+ * Tests {@link RegressionLine} the inner class of {@link AdaptiveTimeout} and contains
+ * all test cases needed to check every method.
+ *
+ * @author Michael Vogt
+ */
+public class RegressionLineTest {
+
+	@Test
+	public void constructor() {
+
+	}
+
+}
