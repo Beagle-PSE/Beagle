14d64b1e369d49222db213e5e40af14aab380a4a
Michael Vogt <mvogt24@googlemail.com>
2016-02-28 16:59:52 +0100

    adapt ConstantTimeoutTests.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/ConstantTimeoutTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/ConstantTimeoutTest.java
@@ -1,9 +1,12 @@
 package de.uka.ipd.sdq.beagle.core.timeout;
 
+import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.BDDMockito.given;
+
+import de.uka.ipd.sdq.beagle.core.testutil.ThrowingMethod;
 
 import org.junit.Test;
 
@@ -15,11 +18,40 @@ import org.junit.Test;
 public class ConstantTimeoutTest {
 
 	/**
+	 * Test method for the constructor.
+	 */
+	@Test
+	public void constructorConstantTimeout() {
+		final int negTimeout = -100;
+		final int valideTimeout = 100;
+		ThrowingMethod method = () -> {
+			final ConstantTimeout constTimeout1 = new ConstantTimeout(negTimeout);
+			constTimeout1.init();
+		};
+		assertThat("timeout must not be negative.", method, throwsException(IllegalArgumentException.class));
+
+		method = () -> {
+			final ConstantTimeout constTimeout2 = new ConstantTimeout(valideTimeout);
+			constTimeout2.init();
+			constTimeout2.init();
+		};
+		assertThat("constTimeout must not be allowed to call init() more than one time.", method,
+			throwsException(IllegalStateException.class));
+
+		method = () -> {
+			new ConstantTimeout(valideTimeout);
+		};
+		assertThat("init() must be called once.", method, throwsException(IllegalStateException.class));
+
+	}
+
+	/**
 	 * Test method for {@link ConstantTimeout#isReached()}.
 	 */
 	@Test
 	public void isReached() {
 		final int timeout = 100;
+
 		final ConstantTimeout constTimeout = new ConstantTimeout(timeout);
 		constTimeout.init();
 		assertThat(constTimeout.isReached(), is(equalTo(false)));
@@ -38,6 +70,7 @@ public class ConstantTimeoutTest {
 	public void getTimeout() {
 		final int timeout = 100;
 		final ConstantTimeout constTimeout = new ConstantTimeout(timeout);
+		constTimeout.init();
 		assertThat(constTimeout.getTimeout(), is((long) timeout));
 
 	}
@@ -49,6 +82,7 @@ public class ConstantTimeoutTest {
 	public void reportOneStepProgress() {
 		final int timeout = 100;
 		final ConstantTimeout constTimeout = new ConstantTimeout(timeout);
+		constTimeout.init();
 		constTimeout.reportOneStepProgress();
 	}
 
