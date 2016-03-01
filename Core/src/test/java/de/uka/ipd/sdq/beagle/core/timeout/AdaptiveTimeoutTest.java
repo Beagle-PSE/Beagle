c134c8ca69df0a377495529d5218e1304183fd83
Michael Vogt <mvogt24@googlemail.com>
2016-03-02 00:10:33 +0100

    adapt some tests.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AdaptiveTimeoutTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AdaptiveTimeoutTest.java
@@ -4,18 +4,29 @@ import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsE
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 import de.uka.ipd.sdq.beagle.core.testutil.ThrowingMethod;
 
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
 
 /**
  * Tests {@link AdaptiveTimeout} and contains all test cases needed to check every method.
  *
  * @author Michael Vogt
  */
-
+@PrepareForTest(AdaptiveTimeout.class)
 public class AdaptiveTimeoutTest {
+
+	/**
+	 * Loads PowerMockito to mock the static final class {@link System}.
+	 */
+	@Rule
+	public final PowerMockRule loadPowerMockito = new PowerMockRule();
 
 	/**
 	 * Test that init() is called at first method and only once.
@@ -41,6 +52,7 @@ public class AdaptiveTimeoutTest {
 	public void reportOneStepProgress() {
 		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
 		adaptTimeout.init();
+		mockStatic(System.class);
 		for (int i = 0; i < 10; i++) {
 			adaptTimeout.reportOneStepProgress();
 		}
@@ -72,6 +84,7 @@ public class AdaptiveTimeoutTest {
 	 */
 	@Test
 	public void isReached() {
+		mockStatic(System.class);
 
 		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
 		adaptTimeout.init();
@@ -102,6 +115,21 @@ public class AdaptiveTimeoutTest {
 		given(System.currentTimeMillis()).willReturn((long) (5 * 60 * 1000 + 10));
 		adaptTimeout3.reportOneStepProgress();
 		assertThat(adaptTimeout3.isReached(), equalTo(true));
+	}
+
+	/**
+	 * Test method for {@link AdaptiveTimeout#notifyOnReachedTimeout()}.
+	 */
+	@Ignore
+	// This test is not ready yet.
+	@Test
+	public void notifyOnReachedTimeout() {
+		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
+		adaptTimeout.init();
+		for (int i = 0; i < 10; i++) {
+			adaptTimeout.reportOneStepProgress();
+		}
+
 	}
 
 }
