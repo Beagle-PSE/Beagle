612f3c8f20381bf845acc3934ff3ef5e113eff8c
Michael Vogt <mvogt24@googlemail.com>
2016-03-01 21:51:29 +0100

    added tests for isReached().

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AdaptiveTimeoutTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AdaptiveTimeoutTest.java
@@ -1,5 +1,11 @@
 package de.uka.ipd.sdq.beagle.core.timeout;
 
+import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+import de.uka.ipd.sdq.beagle.core.testutil.ThrowingMethod;
+
 import org.junit.Test;
 
 /**
@@ -10,12 +16,43 @@ import org.junit.Test;
 public class AdaptiveTimeoutTest {
 
 	/**
-	 * Test method for {@link ConstantTimeout#isReached()}.
+	 * Test that init() is called at first method and only once.
 	 */
 	@Test
 	public void init() {
 		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
 		adaptTimeout.init();
+
+		final AdaptiveTimeout adaptTimeout1 = new AdaptiveTimeout();
+		adaptTimeout1.init();
+		final ThrowingMethod method = () -> {
+			adaptTimeout1.init();
+		};
+		assertThat("adaptTimeout1 must not be allowed to call init() more than one time.", method,
+			throwsException(IllegalStateException.class));
 	}
 
+	/**
+	 * Test method for {@link AdaptiveTimeout#reportOneStepProgress()}.
+	 */
+	@Test
+	public void reportOneStepProgress() {
+	}
+
+	/**
+	 * Test method for {@link AdaptiveTimeout#isReached()}.
+	 */
+	@Test
+	public void isReached() {
+		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
+		adaptTimeout.init();
+		assertThat(adaptTimeout.isReached(), equalTo(false));
+
+		final AdaptiveTimeout adaptTimeout1 = new AdaptiveTimeout();
+		adaptTimeout1.init();
+		for (int i = 0; i < 10; i++) {
+			adaptTimeout1.isReached();
+		}
+		assertThat(adaptTimeout1.isReached(), equalTo(false));
+	}
 }
