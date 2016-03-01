41e5133abc52e62e6111a48e6deba5e02490cd9d
Michael Vogt <mvogt24@googlemail.com>
2016-03-01 23:13:43 +0100

    added some tests for AdaptiveTimeout.

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AdaptiveTimeoutTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AdaptiveTimeoutTest.java
@@ -3,9 +3,11 @@ package de.uka.ipd.sdq.beagle.core.timeout;
 import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
 
 import de.uka.ipd.sdq.beagle.core.testutil.ThrowingMethod;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -13,6 +15,7 @@ import org.junit.Test;
  *
  * @author Michael Vogt
  */
+
 public class AdaptiveTimeoutTest {
 
 	/**
@@ -37,13 +40,32 @@ public class AdaptiveTimeoutTest {
 	 */
 	@Test
 	public void reportOneStepProgress() {
+		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
+		adaptTimeout.init();
+		for (int i = 0; i < 10; i++) {
+			adaptTimeout.reportOneStepProgress();
+		}
+
+		final AdaptiveTimeout adaptTimeout1 = new AdaptiveTimeout();
+		adaptTimeout1.init();
+		adaptTimeout1.reportOneStepProgress();
+
+		final AdaptiveTimeout adaptTimeout2 = new AdaptiveTimeout();
+		adaptTimeout2.init();
+		for (int i = 0; i < 10; i++) {
+			adaptTimeout2.reportOneStepProgress();
+		}
+		given(System.currentTimeMillis()).willReturn((long) (5 * 60 * 1000 + 10));
+		adaptTimeout2.reportOneStepProgress();
 	}
 
 	/**
 	 * Test method for {@link AdaptiveTimeout#isReached()}.
 	 */
+	@Ignore
 	@Test
 	public void isReached() {
+
 		final AdaptiveTimeout adaptTimeout = new AdaptiveTimeout();
 		adaptTimeout.init();
 		assertThat(adaptTimeout.isReached(), equalTo(false));
@@ -51,8 +73,28 @@ public class AdaptiveTimeoutTest {
 		final AdaptiveTimeout adaptTimeout1 = new AdaptiveTimeout();
 		adaptTimeout1.init();
 		for (int i = 0; i < 10; i++) {
-			adaptTimeout1.isReached();
+			adaptTimeout1.reportOneStepProgress();
+
 		}
 		assertThat(adaptTimeout1.isReached(), equalTo(false));
+
+		final AdaptiveTimeout adaptTimeout2 = new AdaptiveTimeout();
+		adaptTimeout2.init();
+		for (int i = 0; i < 10; i++) {
+			adaptTimeout2.reportOneStepProgress();
+		}
+		given(System.currentTimeMillis()).willReturn((long) (5 * 60 * 1000 + 10));
+
+		assertThat(adaptTimeout2.isReached(), equalTo(true));
+
+		final AdaptiveTimeout adaptTimeout3 = new AdaptiveTimeout();
+		adaptTimeout3.init();
+		for (int i = 0; i < 10; i++) {
+			adaptTimeout3.reportOneStepProgress();
+		}
+		given(System.currentTimeMillis()).willReturn((long) (5 * 60 * 1000 + 10));
+		adaptTimeout3.reportOneStepProgress();
+		assertThat(adaptTimeout3.isReached(), equalTo(true));
 	}
+
 }
