13d3800375a1aa772f5c9964701c7517dbcca0fa
m1cm1c <michelbach94@gmail.com>
2016-03-04 16:20:47 +0100

    Implemented AgingAlgorithmAdaptiveTimeout.

--- Core/src/main/java/de/uka/ipd/sdq/beagle/core/timeout/AgingAlgorithmAdaptiveTimeout.java
+++ Core/src/main/java/de/uka/ipd/sdq/beagle/core/timeout/AgingAlgorithmAdaptiveTimeout.java
@@ -9,22 +9,54 @@ package de.uka.ipd.sdq.beagle.core.timeout;
  */
 public class AgingAlgorithmAdaptiveTimeout extends ExecutionTimeBasedTimeout {
 
+	/**
+	 * The alpha value of the aging algorithm.
+	 */
+	private static final double alpha = 0.5;
+
+	/**
+	 * The previous maximally tolerable time in milliseconds or {@code -1} to indicate
+	 * that there is none.
+	 */
+	private long previousMaximallyTolerableTime = -1;
+
+	/**
+	 * The maximally tolerable time in milliseconds or {@code -1} to indicate that there
+	 * is none.
+	 */
+	private long maximallyTolerableTime = -1;
+
+	/**
+	 * When {@link #isReached()} has been called the last time. Stated in milliseconds.
+	 */
+	private long timeOfPreviousCall;
+
 	@Override
 	public boolean isReached() {
-		// TODO Auto-generated method stub
-		return false;
+		if (this.maximallyTolerableTime == -1) {
+			return false;
+		} else {
+			return this.timeOfPreviousCall + this.previousMaximallyTolerableTime - System.currentTimeMillis() < 0;
+		}
 	}
 
 	@Override
 	public void reportOneStepProgress() {
-		// TODO Auto-generated method stub
+		this.previousMaximallyTolerableTime = this.maximallyTolerableTime;
+		final long tellingTime = this.timeOfPreviousCall - System.currentTimeMillis();
+		this.timeOfPreviousCall = System.currentTimeMillis();
 
+		if (this.previousMaximallyTolerableTime == -1) {
+			this.maximallyTolerableTime = tellingTime;
+		} else {
+			this.maximallyTolerableTime =
+				(long) (this.previousMaximallyTolerableTime * (1 - alpha) + tellingTime * alpha);
+		}
 	}
 
 	@Override
 	protected void implementationInit() {
-		// TODO Auto-generated method stub
-
+		this.timeOfPreviousCall = System.currentTimeMillis();
 	}
 
 }
