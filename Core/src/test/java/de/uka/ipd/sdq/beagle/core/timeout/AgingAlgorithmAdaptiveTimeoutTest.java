1b1893e92ba4e39eff0786f61241d23833b2cd35
Michael Vogt <mvogt24@googlemail.com>
2016-03-05 15:39:22 +0100

    created test class for AgingAlgorithmAdaptiveTimeout and added some tests.

---
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/timeout/AgingAlgorithmAdaptiveTimeoutTest.java
@@ -0,0 +1,75 @@
+package de.uka.ipd.sdq.beagle.core.timeout;
+
+import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+
+import de.uka.ipd.sdq.beagle.core.testutil.ThrowingMethod;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+
+/**
+ * Tests {@link AgingAlgorithmAdaptiveTimeout} and contains all test cases needed to check
+ * every method.
+ *
+ * @author Michael Vogt
+ */
+@PrepareForTest(AgingAlgorithmAdaptiveTimeout.class)
+public class AgingAlgorithmAdaptiveTimeoutTest {
+
+	/**
+	 * Loads PowerMockito to mock the static final class {@link System}.
+	 */
+	@Rule
+	public final PowerMockRule loadPowerMockito = new PowerMockRule();
+
+	/**
+	 * Test method for {@link AgingAlgorithmAdaptiveTimeout#isReached()}.
+	 */
+	@Test
+	public void isReached() {
+		final AgingAlgorithmAdaptiveTimeout adaptTimeout = new AgingAlgorithmAdaptiveTimeout();
+		mockStatic(System.class);
+		given(System.currentTimeMillis()).willReturn(0L);
+		adaptTimeout.implementationInit();
+		assertThat(adaptTimeout.isReached(), equalTo(false));
+
+		final AgingAlgorithmAdaptiveTimeout adaptTimeout2 = new AgingAlgorithmAdaptiveTimeout();
+		adaptTimeout2.implementationInit();
+		given(System.currentTimeMillis()).willReturn(1000L);
+		assertThat(adaptTimeout2.isReached(), equalTo(true));
+	}
+
+	/**
+	 * Test method for {@link AgingAlgorithmAdaptiveTimeout#reportOneStepProgress()}.
+	 */
+	@Test
+	public void reportOneStepProgress() {
+		final AgingAlgorithmAdaptiveTimeout adaptTimeout = new AgingAlgorithmAdaptiveTimeout();
+		adaptTimeout.implementationInit();
+		adaptTimeout.reportOneStepProgress();
+	}
+
+	/**
+	 * Test that init() is called at first method and only once.
+	 */
+	@Test
+	public void init() {
+		final AgingAlgorithmAdaptiveTimeout adaptTimeout = new AgingAlgorithmAdaptiveTimeout();
+		adaptTimeout.implementationInit();
+
+		final AgingAlgorithmAdaptiveTimeout adaptTimeout1 = new AgingAlgorithmAdaptiveTimeout();
+		adaptTimeout1.init();
+		final ThrowingMethod method = () -> {
+			adaptTimeout1.implementationInit();
+		};
+		assertThat("adaptTimeout1 must not be allowed to call init() more than one time.", method,
+			throwsException(IllegalStateException.class));
+	}
+
+}
