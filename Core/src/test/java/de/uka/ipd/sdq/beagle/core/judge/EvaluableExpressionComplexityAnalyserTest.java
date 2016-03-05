78be736101af7f82e29827a7ef562bd124be4334
Annika Berger <annika.c.berger@gmail.com>
2016-03-05 16:39:20 +0100

    Test for trying to read value before calculating

--- Core/src/test/java/de/uka/ipd/sdq/beagle/core/judge/EvaluableExpressionComplexityAnalyserTest.java
+++ Core/src/test/java/de/uka/ipd/sdq/beagle/core/judge/EvaluableExpressionComplexityAnalyserTest.java
@@ -1,17 +1,24 @@
 package de.uka.ipd.sdq.beagle.core.judge;
 
+import static de.uka.ipd.sdq.beagle.core.testutil.ExceptionThrownMatcher.throwsException;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.Assert.assertThat;
 
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.AdditionExpression;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.ComparisonExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.ConstantExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.DivisionExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.EvaluableExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.EvaluableVariable;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.ExponentationExpression;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.ExponentialFunctionExpression;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.IfThenElseExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.LogarithmExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.MultiplicationExpression;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.NaturalLogarithmExpression;
 import de.uka.ipd.sdq.beagle.core.evaluableexpressions.SineExpression;
+import de.uka.ipd.sdq.beagle.core.evaluableexpressions.SubtractionExpression;
 
 import org.junit.Test;
 
@@ -59,6 +66,39 @@ public class EvaluableExpressionComplexityAnalyserTest {
 		analyser.determineComplexity(complex);
 		final double complexValue2 = analyser.getHumanComprehensibilityComplexitySum();
 		assertThat(complexValue, is(complexValue2));
+
+		simple = new SubtractionExpression(
+			new DivisionExpression(new EvaluableVariable("a"),
+				new LogarithmExpression(ConstantExpression.forValue(2),
+					new SineExpression(
+						new DivisionExpression(ConstantExpression.forValue(4.32), new EvaluableVariable("c"))))),
+			new ExponentationExpression(ConstantExpression.forValue(393.123),
+				new DivisionExpression(ConstantExpression.forValue(-1),
+					new IfThenElseExpression(
+						new ComparisonExpression(new EvaluableVariable("z"), ConstantExpression.forValue(0)),
+						new NaturalLogarithmExpression(new SineExpression(ConstantExpression.forValue(1))),
+						new ExponentialFunctionExpression(new EvaluableVariable("g"))))));
+		complex =
+			new AdditionExpression(ConstantExpression.forValue(-1.0), ConstantExpression.forValue(1.0),
+				new SubtractionExpression(
+					new DivisionExpression(new EvaluableVariable("a"),
+						new LogarithmExpression(ConstantExpression.forValue(2),
+							new SineExpression(new DivisionExpression(ConstantExpression.forValue(4.32),
+								new EvaluableVariable("c"))))),
+				new ExponentationExpression(ConstantExpression.forValue(393.123),
+					new DivisionExpression(ConstantExpression.forValue(-1),
+						new IfThenElseExpression(
+							new ComparisonExpression(new EvaluableVariable("z"), ConstantExpression.forValue(0)),
+							new NaturalLogarithmExpression(new SineExpression(ConstantExpression.forValue(1))),
+							new ExponentialFunctionExpression(new EvaluableVariable("g")))))));
+		analyser.determineComplexity(simple);
+		simpleValue = analyser.getHumanComprehensibilityComplexitySum();
+		analyser.determineComplexity(complex);
+		complexValue = analyser.getHumanComprehensibilityComplexitySum();
+		assertThat(simpleValue, is(lessThan(complexValue)));
+		
+		final EvaluableExpressionComplexityAnalyser newAnalyser = new EvaluableExpressionComplexityAnalyser();
+		assertThat(() -> newAnalyser.getHumanComprehensibilityComplexitySum(), throwsException(IllegalStateException.class));
 	}
 
 	/**
@@ -97,6 +137,28 @@ public class EvaluableExpressionComplexityAnalyserTest {
 		analyser.determineComplexity(complex);
 		final double complexValue2 = analyser.getComputationalComplexitySum();
 		assertThat(complexValue, is(complexValue2));
+		
+		complex =
+			new AdditionExpression(ConstantExpression.forValue(-1.0), ConstantExpression.forValue(1.0),
+				new SubtractionExpression(
+					new DivisionExpression(new EvaluableVariable("a"),
+						new LogarithmExpression(ConstantExpression.forValue(2),
+							new SineExpression(new DivisionExpression(ConstantExpression.forValue(4.32),
+								new EvaluableVariable("c"))))),
+				new ExponentationExpression(ConstantExpression.forValue(393.123),
+					new DivisionExpression(ConstantExpression.forValue(-1),
+						new IfThenElseExpression(
+							new ComparisonExpression(new EvaluableVariable("z"), ConstantExpression.forValue(0)),
+							new NaturalLogarithmExpression(new SineExpression(ConstantExpression.forValue(1))),
+							new ExponentialFunctionExpression(new EvaluableVariable("g")))))));
+		analyser.determineComplexity(simple);
+		simpleValue = analyser.getComputationalComplexitySum();
+		analyser.determineComplexity(complex);
+		complexValue = analyser.getComputationalComplexitySum();
+		assertThat(simpleValue, is(lessThan(complexValue)));
+		
+		final EvaluableExpressionComplexityAnalyser newAnalyser = new EvaluableExpressionComplexityAnalyser();
+		assertThat(() -> newAnalyser.getComputationalComplexitySum(), throwsException(IllegalStateException.class));
 	}
 
 }
